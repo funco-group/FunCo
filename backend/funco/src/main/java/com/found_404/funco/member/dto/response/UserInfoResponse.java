e29dfd0d8f359863452f71410c87bec29c8fb248
이선주 <sunju5402@naver.com>
2024-04-25 18:04:12 +0900

    feat: 유저페이지 응답 dto 구현

---
+++ backend/funco/src/main/java/com/found_404/funco/member/dto/response/UserInfoResponse.java
@@ -0,0 +1,27 @@
+package com.found_404.funco.member.dto.response;
+
+import java.util.List;
+
+import com.found_404.funco.member.dto.MemberAssetInfo;
+import com.found_404.funco.trade.dto.RecentTradedCoin;
+
+import lombok.Builder;
+
+@Builder
+public record UserInfoResponse(
+	Long memberId,
+	String nickname,
+	String profileUrl,
+	String introduction,
+	Long assetRank,
+	Long followingCashRank,
+	MemberAssetInfo memberAssetInfo,
+	List<RecentTradedCoin> topCoins,
+	Long followingCash,
+	Long followerCash,
+	Boolean isFollow,
+	String portfolioStatus,
+	Long portfolioPrice,
+	Long badgeId
+) {
+}
