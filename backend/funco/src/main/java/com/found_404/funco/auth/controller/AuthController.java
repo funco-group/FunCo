f57443789b720e781c686fcafd3956f7aa4fa167
박세웅 <ert4263@gmail.com>
2024-04-26 13:29:45 +0900

    add: 토큰 재발급 관련 Controller와 Service에 메서드 작성

--- backend/funco/src/main/java/com/found_404/funco/auth/controller/AuthController.java
+++ backend/funco/src/main/java/com/found_404/funco/auth/controller/AuthController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.found_404.funco.auth.dto.response.LoginResponse;
+import com.found_404.funco.auth.dto.response.TokenResponse;
 import com.found_404.funco.auth.service.AuthService;
 import com.found_404.funco.auth.type.OauthServerType;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -27,4 +29,14 @@ public class AuthController {
 		@RequestParam String code) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(authService.login(response, provider, code));
 	}
+
+	// 토큰 재발급 API 구현 필요
+	// accessToken만 리턴해주면 된다.
+	// accessToken을 뜯어서 expired 여부를 체크한 다음, expired 되었다면 클라이언트에 재발급 해야한다고 요청 보낼 필요 있음
+		// 스프링 시큐리티에서 가능할 듯
+	@PostMapping("/reissue")
+	public ResponseEntity<TokenResponse> reissueToken(HttpServletRequest request, HttpServletResponse response){
+		return ResponseEntity.status(HttpStatus.CREATED).body(authService.reissueToken(request, response));
+	}
+
 }
