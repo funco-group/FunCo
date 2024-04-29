5d3fed75efc4ecfa5ca43d82b7ab5593fbab77a2
이선주 <sunju5402@naver.com>
2024-04-29 14:18:50 +0900

    feat: 포트폴리오 공개 여부 로직 구현

---
+++ backend/funco/src/main/java/com/found_404/funco/portfolio/service/PortfolioService.java
@@ -0,0 +1,29 @@
+package com.found_404.funco.portfolio.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.found_404.funco.member.domain.Member;
+import com.found_404.funco.member.domain.repository.MemberRepository;
+import com.found_404.funco.member.domain.type.PortfolioStatusType;
+import com.found_404.funco.member.exception.MemberErrorCode;
+import com.found_404.funco.member.exception.MemberException;
+import com.found_404.funco.portfolio.dto.request.PortfolioStatusRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PortfolioService {
+	private final MemberRepository memberRepository;
+
+	@Transactional
+	public void updatePortfolioStatus(Long memberId, PortfolioStatusRequest portfolioStatusRequest) {
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
+
+		member.updatePortfolioStatus(
+			PortfolioStatusType.valueOf(portfolioStatusRequest.portfolioStatus().toUpperCase()));
+		member.updatePortfolioPrice(portfolioStatusRequest.portfolioPrice());
+	}
+}
