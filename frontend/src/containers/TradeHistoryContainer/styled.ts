54000f8cfffc844c746132d9cb4880b51f7ece1c
황주영 <ghkdwndud7@naver.com>
2024-04-29 20:59:08 +0900

    feat: historyPage 탭까지 완성

---
+++ frontend/src/containers/TradeHistoryContainer/styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { CommonComponent } from '@/styles/CommonStyled'
+
+const TradeHistoryPageContainer = styled.div`
+  ${CommonComponent}
+`
+
+export default TradeHistoryPageContainer
