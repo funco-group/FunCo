6543a5edc6e8cac1fcb5782c87a5cb764de29e83
이선주 <sunju5402@naver.com>
2024-05-02 08:47:11 +0900

    feat: seller 및 subscriber의 팔로워를 찾는 querydsl 작성

---
+++ backend/funco/src/main/java/com/found_404/funco/portfolio/domain/repository/SubscribeCustomRepository.java
@@ -0,0 +1,9 @@
+package com.found_404.funco.portfolio.domain.repository;
+
+import java.util.List;
+
+import com.found_404.funco.portfolio.dto.FollowerInfo;
+
+public interface SubscribeCustomRepository {
+	List<FollowerInfo> findFollowInfoByFollowingId(Long followingId);
+}
