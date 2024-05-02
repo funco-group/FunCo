1d6b5a22da4264c21114f205a7d2e858e3d7bb5e
황주영 <ghkdwndud7@naver.com>
2024-05-02 16:46:21 +0900

    feat: chart components 옮기기 + 수정

---
+++ frontend/src/components/Common/Chart/ColumnChart.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+import palette from '@/lib/palette'
+import { ChartLabel } from './Chart.styled'
+
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false })
+
+interface ColumnChartProps {
+  dataSet: (string | number)[][]
+  chartName: string
+  unit: string
+}
+
+function ColumnChart({ dataSet, chartName, unit }: ColumnChartProps) {
+  const [chartData] = useState({
+    series: [
+      {
+        name: 'Cash Flow',
+        data: dataSet.map((data) => Number(data[1])),
+      },
+    ],
+    options: {
+      chart: {
+        type: 'bar' as const,
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          colors: {
+            ranges: [
+              {
+                from: 0,
+                to: 100000000000000,
+                color: palette.brandRed,
+              },
+              {
+                from: 0,
+                to: -100000000000000,
+                color: palette.brandBlue,
+              },
+            ],
+          },
+          columnWidth: '80%',
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      yaxis: {
+        labels: {
+          formatter(y: number) {
+            return `${y.toLocaleString('ko-KR')}`
+          },
+        },
+      },
+      xaxis: {
+        type: 'datetime' as const,
+        categories: dataSet.map((data) => data[0]),
+        labels: {
+          rotate: -90,
+        },
+      },
+    },
+  })
+
+  return (
+    <div>
+      <ChartLabel>
+        <div>{chartName}</div>
+        <div>단위: {unit}</div>
+      </ChartLabel>
+      <ReactApexChart
+        options={chartData.options}
+        series={chartData.series}
+        type="bar"
+      />
+    </div>
+  )
+}
+
+export default ColumnChart
