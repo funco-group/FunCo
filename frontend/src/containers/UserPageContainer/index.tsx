efa2b98f16f061a11e32d2849ee2bb980d4fd6c8
황주영 <ghkdwndud7@naver.com>
2024-05-02 16:44:57 +0900

    feat: userPageContainer 추가

---
+++ frontend/src/containers/UserPageContainer/index.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import useFollowModalState from '@/hooks/recoilHooks/useFollowModalState'
+import useUserState from '@/hooks/recoilHooks/useUserState'
+import { MemberType } from '@/interfaces/userPage/MemberType'
+import { getMemberInfo } from '@/apis/member'
+import FollowModal from '@/components/UserPage/FollowModal'
+import UserPageProfile from '@/components/UserPage/UserPageProfile'
+import AssetGraph from '@/components/UserPage/AssetGraph'
+import RecentInvestment from '@/components/UserPage/RecentInvestment'
+import UserFollow from '@/components/UserPage/UserFollow'
+import ReturnRateGraph from '@/components/UserPage/ReturnRateGraph'
+import { UserLayoutRowDiv } from './styled'
+
+function UserPageContainer({ memberId }: { memberId: number }) {
+  const { user } = useUserState()
+  const [member, setMember] = useState<MemberType>()
+  const { followModal } = useFollowModalState()
+
+  useEffect(() => {
+    if (memberId) {
+      getMemberInfo(+memberId, (res) => {
+        const { data } = res
+        setMember(data)
+      })
+    }
+  }, [memberId])
+
+  if (!memberId || !user || !member) {
+    return <div>존재하지 않는 회원입니다.</div>
+  }
+
+  const isCurrentUser = user?.memberId === +memberId
+  return (
+    <div>
+      {followModal && member && (
+        <FollowModal member={member} setMember={setMember} />
+      )}
+      <UserLayoutRowDiv>
+        <UserPageProfile isCurrentUser={isCurrentUser} member={member} />
+        <AssetGraph member={member} />
+      </UserLayoutRowDiv>
+      <UserLayoutRowDiv>
+        <RecentInvestment topCoins={member.topCoins} />
+        <UserFollow
+          followingCash={member.followingCash}
+          followerCash={member.followerCash}
+        />
+      </UserLayoutRowDiv>
+      <ReturnRateGraph memberId={member.memberId} />
+    </div>
+  )
+}
+
+export default UserPageContainer
