1c4ad270851ffd6b9d22457fd2ed4c86ed015da9
엄소현 <sohy19@hufs.ac.kr>
2024-05-03 17:05:16 +0900

    fix: 댓글 업데이트 유저 검증 로직 수정

--- backend/funco/src/main/java/com/found_404/funco/note/service/NoteCommentService.java
+++ backend/funco/src/main/java/com/found_404/funco/note/service/NoteCommentService.java
@@ -1,12 +1,15 @@
 package com.found_404.funco.note.service;
 
+import static com.found_404.funco.member.exception.MemberErrorCode.INVALID_MEMBER;
 import static com.found_404.funco.note.exception.NoteCommentErrorCode.NOT_FOUND_NOTE_COMMENT;
 
 import com.found_404.funco.member.domain.Member;
+import com.found_404.funco.member.exception.MemberException;
 import com.found_404.funco.note.domain.NoteComment;
 import com.found_404.funco.note.domain.repository.NoteCommentRepository;
 import com.found_404.funco.note.dto.request.EditNoteCommentRequest;
 import com.found_404.funco.note.exception.NoteCommentException;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,8 +23,11 @@ public class NoteCommentService {
     @Transactional
     public void editComment(Member member, Long commentId, EditNoteCommentRequest request) {
         NoteComment comment = noteCommentRepository.findById(commentId).orElseThrow(() -> new NoteCommentException(NOT_FOUND_NOTE_COMMENT));
-        if (comment.getMember().getId().equals(member.getId())) {
+        if (Objects.nonNull(member) && comment.getMember().getId().equals(member.getId())) {
             comment.editNoteComment(request.content());
+        }
+        else {
+            throw new MemberException(INVALID_MEMBER);
         }
     }
 }
