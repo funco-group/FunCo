247a9d6d779a29a7cda2af014ac9eb58903ccb7f
황주영 <ghkdwndud7@naver.com>
2024-05-04 17:53:59 +0900

    feat: sorted, 글작성 버튼 구현

--- frontend/src/components/Common/Button/TabButton.styled.ts
+++ frontend/src/components/Common/Button/TabButton.styled.ts
@@ -1,57 +1,58 @@
-import styled from "styled-components";
-import palette from "@/lib/palette";
+import styled from 'styled-components'
+import palette from '@/lib/palette'
 
 interface TabButtonProps {
-  width: string | null;
-  height: string;
-  $active: boolean;
-  radius: "left" | "right" | "";
+  width: string | null
+  height: string
+  $active: boolean
+  radius: 'left' | 'right' | ''
 }
 
 const TabButton = styled.button<TabButtonProps>`
+  box-sizing: border-box;
   background-color: transparent;
-  width: ${({ width }) => width || null};
+  width: ${({ width }) => width || 'auto'};
   height: ${({ height }) => height};
-  color: ${({ $active }) => ($active ? palette.brandColor : "#999999")};
-  padding: 0.5rem;
+  color: ${({ $active }) => ($active ? palette.brandColor : '#999999')};
+  padding: 0.4rem;
   border-top: ${({ $active }) =>
     $active
-      ? `1px solid ${palette.brandColor}`
-      : `1px solid ${palette.deactivatedGray}`};
+      ? `2px solid ${palette.brandColor}`
+      : `2px solid ${palette.deactivatedGray}`};
   border-bottom: ${({ $active }) =>
     $active
-      ? `1px solid ${palette.brandColor}`
-      : `1px solid ${palette.deactivatedGray}`};
+      ? `2px solid ${palette.brandColor}`
+      : `2px solid ${palette.deactivatedGray}`};
   border-left: ${({ $active, radius }) => {
     if ($active) {
-      return `1px solid ${palette.brandColor}`;
+      return `2px  solid ${palette.brandColor}`
     }
-    if (radius === "right") {
-      return "none";
+    if (radius === 'right') {
+      return 'none'
     }
-    return `1px solid ${palette.deactivatedGray}`;
+    return `2px  solid ${palette.deactivatedGray}`
   }};
   border-right: ${({ $active, radius }) => {
     if ($active) {
-      return `1px solid ${palette.brandColor}`;
+      return `2px  solid ${palette.brandColor}`
     }
-    if (radius === "left") {
-      return "none";
+    if (radius === 'left') {
+      return 'none'
     }
-    return `1px solid ${palette.deactivatedGray}`;
+    return `2px  solid ${palette.deactivatedGray}`
   }};
   border-radius: ${({ radius }) => {
-    if (radius === "left") {
-      return "5px 0 0 5px";
+    if (radius === 'left') {
+      return '5px 0 0 5px'
     }
-    if (radius === "right") {
-      return "0 5px 5px 0";
+    if (radius === 'right') {
+      return '0 5px 5px 0'
     }
-    return null;
+    return '0'
   }};
   font-family: ${({ $active }) =>
-    $active ? "NanumSquareBold" : "NanumSquare"};
+    $active ? 'NanumSquareBold' : 'NanumSquare'};
   cursor: pointer;
-`;
+`
 
-export default TabButton;
+export default TabButton
--- frontend/src/containers/NotesContainer/index.tsx
+++ frontend/src/containers/NotesContainer/index.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import TabButton from '@/components/Common/Button/TabButton.styled'
 import NotesFilterBtnList from '@/components/Note/Notes/NotesFilterBtnList'
 import NotesSearch from '@/components/Note/Notes/NotesSearch'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
 function Notes() {
@@ -9,6 +11,21 @@ function Notes() {
   const [coinList, setCoinList] = useState<string[]>([])
   const [search, setSearch] = useState<string>('')
   const [keyword, setKeyword] = useState<string>('')
+  const [sorted, setSorted] = useState<string>('LATEST')
+  const router = useRouter()
+
+  const sortedList: [string, string, 'left' | 'right' | ''][] = [
+    ['최신 순', 'LATEST', 'left'],
+    ['추천 순', 'RECOMMENDED', 'right'],
+  ]
+
+  const handleTabClick = (tabName: string) => {
+    setSorted(tabName)
+  }
+
+  const handleWriteClick = () => {
+    router.push('/note/write')
+  }
 
   return (
     <div>
@@ -19,15 +36,40 @@ function Notes() {
           coinList={coinList}
           setCoinList={setCoinList}
         />
-        <NotesSearch
-          search={search}
-          setSearch={setSearch}
-          keyword={keyword}
-          setKeyword={setKeyword}
-        />
+        <NotesSearch setSearch={setSearch} setKeyword={setKeyword} />
       </div>
-      <div></div>
-      <div></div>
+      <div className="mt-3 flex justify-between">
+        <div>
+          {sortedList.map((item) => (
+            <TabButton
+              width={null}
+              height="35px"
+              $active={item[1] === sorted}
+              radius={item[2]}
+              onClick={() => handleTabClick(item[1])}
+              key={item[1]}
+            >
+              {item[0]}
+            </TabButton>
+          ))}
+        </div>
+        <button
+          type="button"
+          className="block cursor-pointer rounded border-none bg-brandColor px-5 py-3 font-NSB text-xs text-brandWhite"
+          onClick={handleWriteClick}
+        >
+          글 작성
+        </button>
+      </div>
+      <div className="grid grid-cols-3 gap-8">
+        <div className="h-11 bg-black" />
+        <div className="h-11 bg-black" />
+        <div className="h-11 bg-black" />
+        <div className="h-11 bg-black" />
+        <div className="h-11 bg-black" />
+        <div className="h-11 bg-black" />
+        <div className="h-11 bg-black" />
+      </div>
     </div>
   )
 }
