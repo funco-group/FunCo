24a6c859b9d593e77fb0be01be0abbc71317d048
소재열 <devjy0309@gmail.com>
2024-05-06 02:55:15 +0900

    init: eureka project 생성

---
+++ backend/funco_eureka/src/test/java/org/found_404/funco_eureka/FuncoEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.found_404.funco_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncoEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
