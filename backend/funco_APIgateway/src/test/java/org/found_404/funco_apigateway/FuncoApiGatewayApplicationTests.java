02f6c0b7b422234cbc6ad519b823cd947fa8026e
소재열 <devjy0309@gmail.com>
2024-05-07 00:58:14 +0900

    init: spring cloud api gateway project init

---
+++ backend/funco_APIgateway/src/test/java/org/found_404/funco_apigateway/FuncoApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.found_404.funco_apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncoApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
