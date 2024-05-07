3543f381c1e2e699313c16e92686b3a6515df814
황주영 <ghkdwndud7@naver.com>
2024-05-07 17:44:36 +0900

    fix: navBar 투자노트 링크 기본 쿼리 파라미터 추가

--- frontend/src/lib/DummyNote.ts
+++ frontend/src/lib/DummyNote.ts
@@ -1,7 +1,7 @@
 const DummyNotes = [
   {
     noteId: 1,
-    nickname: '지구를 지켜츄',
+    nickname: '지구를 지켜야 하니?',
     profileImage: '/image/chuu.gif',
     thumbnail: '/image/hanni-hat.gif',
     title: '지구는 어떻게 지킬까?',
@@ -34,7 +34,7 @@ const DummyNotes = [
     thumbnail: '/image/hanni-nailchew.gif',
     title: '지구는 어떻게 지킬까?',
     content:
-      '## Pre-rendering?\n- 프리렌더링이 뭔데?테스트용 테스트용 테스트용 테스테스텟터세트세트스fsdfsfdsfdsfsd\n ### 글쎄? \n 이것은 테스트\n 이것은 테스트\n 이것은 테스트\n 이것은 테스트\n 이것은 테스트\n 이것은 테스트\n 이것은 테스트 ## Pre-rendering?\n## Pre-rendering?\n## Pre-rendering?\n## Pre-rendering?\n',
+      '## Pre-rendering?\n- 프리렌더링이 뭔데?테스트용 테스트용 테스트용 테스테스텟터세트세트스fs테',
     coinName: '비트코인',
     writeDate: '2024-01-20T15:00:00',
     likeCount: 4,
