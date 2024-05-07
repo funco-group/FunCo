65a5b46c587a922374dbaf4da86b354ceea15050
박세웅 <ert4263@gmail.com>
2024-05-07 17:56:50 +0900

    feat: 원 초기화 컨트롤러 작성

--- backend/funco/src/main/java/com/found_404/funco/asset/controller/AssetController.java
+++ backend/funco/src/main/java/com/found_404/funco/asset/controller/AssetController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,4 +55,9 @@ public class AssetController {
 		return ResponseEntity.ok(assetService.getMemberHistory(member));
 	}
 
+	@PatchMapping("/init-cash")
+	public ResponseEntity<Void> initializeMemberCash(@AuthenticationPrincipal Member member){
+		assetService.initializeMemberCash(member);
+		return ResponseEntity.ok().build();
+	}
 }
