3dcea90d2c2c0c9c2ec1437d1fd8ded5f5add01a
이태호 <leetag1163@naver.com>
2024-05-09 17:14:10 +0900

    feat: JWT 검증 서비스 구현

---
+++ backend/funco_APIgateway/src/main/java/org/found_404/funco_apigateway/security/service/TokenService.java
@@ -0,0 +1,60 @@
+package org.found_404.funco_apigateway.security.service;
+
+import static org.found_404.funco_apigateway.security.exception.TokenErrorCode.*;
+
+import java.util.Base64;
+import java.util.Date;
+
+import org.found_404.funco_apigateway.security.exception.TokenException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TokenService {
+
+	@Value("${spring.jwt.token.secret-key}")
+	private String secretKey;
+
+	@Value("${spring.jwt.token.refresh-secret-key}")
+	private String refreshSecretKey;
+
+	@PostConstruct
+	protected void init() {
+		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+		refreshSecretKey = Base64.getEncoder().encodeToString(refreshSecretKey.getBytes());
+	}
+
+	public String validateToken(String token) {
+		try {
+			Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date();
+
+			if (expirationDate.before(now)) {
+				throw new TokenException(EXPIRED_TOKEN, HttpStatus.UNAUTHORIZED);
+			}
+
+			return claims.getSubject();
+
+		} catch (SecurityException | MalformedJwtException | UnsupportedJwtException e) {
+			throw new TokenException(INVALID_TOKEN, HttpStatus.UNAUTHORIZED);
+		} catch (ExpiredJwtException e) {
+			throw new TokenException(EXPIRED_TOKEN, HttpStatus.UNAUTHORIZED);
+		} catch (IllegalArgumentException e) {
+			throw new TokenException(SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
