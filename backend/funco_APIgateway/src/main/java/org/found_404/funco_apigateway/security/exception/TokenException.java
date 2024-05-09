796844b104beaf42309dcb4c3790bbb8dd719784
이태호 <leetag1163@naver.com>
2024-05-09 17:14:20 +0900

    feat: JWT 예외 처리 구현

---
+++ backend/funco_APIgateway/src/main/java/org/found_404/funco_apigateway/security/exception/TokenException.java
@@ -0,0 +1,18 @@
+package org.found_404.funco_apigateway.security.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public class TokenException extends RuntimeException {
+	private final TokenErrorCode errorCode;
+	private final String errorMessage;
+	private final HttpStatus httpStatus;
+
+	public TokenException(TokenErrorCode errorCode, HttpStatus httpStatus) {
+		this.errorCode = errorCode;
+		this.errorMessage = errorCode.getMessage();
+		this.httpStatus = httpStatus;
+	}
+}
