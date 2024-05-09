b329ef7cf3f1699c14692496a66aa1d782a68275
소재열 <devjy0309@gmail.com>
2024-05-09 16:58:27 +0900

    feat: GlobalLoggingFilter 추가

--- backend/funco_APIgateway/src/main/java/org/found_404/funco_apigateway/config/RouteConfig.java
+++ backend/funco_APIgateway/src/main/java/org/found_404/funco_apigateway/config/RouteConfig.java
@@ -15,6 +15,9 @@ public class RouteConfig {
 			.route(r -> r.path("/user/**")
 				.filters(f -> f.addRequestHeader("Authorization ","Bearer "+"access token"))
 				.uri("http://localhost:8081"))
+			.route(r -> r.path("/user/**")
+				.filters(f -> f.addRequestHeader("member-id","1"))
+				.uri("lb://MSA-USER-SERVICE")) // eureka를 사용하는 경우 lb(loadbalancing)://유레카에서뜨는서비스이름
 			.build();
 	}
 
