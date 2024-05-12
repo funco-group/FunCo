d7591e86f93ececb976b566f14781236d2a0f8ea
이태호 <leetag1163@naver.com>
2024-05-12 20:06:09 +0900

    fix: CORS Filter 수정

--- backend/funco_APIgateway/src/main/java/org/found_404/funco_apigateway/filter/GlobalCORSFilter.java
+++ backend/funco_APIgateway/src/main/java/org/found_404/funco_apigateway/filter/GlobalCORSFilter.java
@@ -1,5 +1,9 @@
 package org.found_404.funco_apigateway.filter;
 
+import static org.springframework.http.HttpHeaders.*;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -17,27 +21,30 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class GlobalCORSFilter {
 
+	private static final List<String> ALLOWED_ORIGINS = List.of("https://www.funco.co.kr", "https://funco.co.kr");
 	private static final String ALLOWED_HEADERS = "x-requested-with, Authorization, Content-Type";
 	private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
-	private static final String ALLOWED_ORIGINS = "https://www.funco.co.kr, https://funco.co.kr";
 	private static final String MAX_AGE = "3600";
 	private static final String ALLOWED_CREDENTIALS = "true";
 
 	@Bean
 	public WebFilter corsFilter() {
-
 		return (ServerWebExchange ctx, WebFilterChain chain) -> {
-
 			ServerHttpRequest request = ctx.getRequest();
+			String requestOrigin = request.getHeaders().getOrigin();
 
 			if (CorsUtils.isPreFlightRequest(request)) {
 				ServerHttpResponse response = ctx.getResponse();
 				HttpHeaders headers = response.getHeaders();
-				headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGINS);
-				headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-				headers.add("Access-Control-Max-Age", MAX_AGE);
-				headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
-				headers.add("Access-Control-Allow-Credentials", ALLOWED_CREDENTIALS);
+
+				if (ALLOWED_ORIGINS.contains(requestOrigin)) {
+					headers.set(ACCESS_CONTROL_ALLOW_ORIGIN, requestOrigin);
+				}
+
+				headers.add(ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHODS);
+				headers.add(ACCESS_CONTROL_MAX_AGE, MAX_AGE);
+				headers.add(ACCESS_CONTROL_ALLOW_HEADERS, ALLOWED_HEADERS);
+				headers.add(ACCESS_CONTROL_ALLOW_CREDENTIALS, ALLOWED_CREDENTIALS);
 
 				if (request.getMethod() == HttpMethod.OPTIONS) {
 					response.setStatusCode(HttpStatus.OK);
