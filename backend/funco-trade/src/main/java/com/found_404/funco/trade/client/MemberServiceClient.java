6a863d0ce5ddf59d802fe5cd35232b45288a804b
devjy39 <devjy0309@gmail.com>
2024-05-13 07:38:42 +0900

    fix: cors 수정, member api 연결 테스트

--- backend/funco-trade/src/main/java/com/found_404/funco/trade/client/MemberServiceClient.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/trade/client/MemberServiceClient.java
@@ -2,6 +2,7 @@ package com.found_404.funco.trade.client;
 
 import com.found_404.funco.trade.client.dto.RequestUpdateCash;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,4 +13,7 @@ public interface MemberServiceClient {
     @PatchMapping(value = "/api/v1/members/{memberId}/cash", produces = "application/json")
     void updateCash(@PathVariable("memberId") Long memberId, @RequestBody RequestUpdateCash updateCash);
     // 부족 시 bad request
+
+    @GetMapping("/api/v1/hello")
+    String hello();
 }
---
+++ backend/funco-trade/src/main/java/com/found_404/funco/trade/controller/MemberHelloController.java
@@ -0,0 +1,25 @@
+package com.found_404.funco.trade.controller;
+
+import com.found_404.funco.trade.client.MemberServiceClient;
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberHelloController {
+    private final MemberServiceClient memberServiceClient;
+
+    @GetMapping("/hello")
+    public String hello() {
+        try {
+            return memberServiceClient.hello();
+        } catch (FeignException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return "error";
+    }
+}
