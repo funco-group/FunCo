59835773e48c8d809f150a213523ed346246997e
devjy39 <devjy0309@gmail.com>
2024-05-13 04:56:30 +0900

    feat: feign logger 설정

--- backend/funco-trade/src/main/java/com/found_404/funco/FuncoTradeApplication.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/FuncoTradeApplication.java
@@ -1,8 +1,13 @@
 package com.found_404.funco;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+
+@EnableFeignClients
 @SpringBootApplication
 public class FuncoTradeApplication {
 
@@ -10,4 +15,8 @@ public class FuncoTradeApplication {
 		SpringApplication.run(FuncoTradeApplication.class, args);
 	}
 
+	@Bean
+	public Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
 }
