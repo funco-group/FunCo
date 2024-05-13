2b761fae1867e84fc4ff73dabf61c11521da31d2
이태호 <leetag1163@naver.com>
2024-05-13 14:04:26 +0900

    fix: cors 수정

--- backend/funco-member/src/main/java/com/found_404/funcomember/global/config/WebMvcConfig.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/global/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.found_404.funcomember.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.found_404.funcomember.global.util.OauthServerTypeConverter;
@@ -12,16 +11,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	// private final AuthMemberIdArgumentResolver authMemberIdArgumentResolver;
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-			.allowedOrigins("https://funco.co.kr", "https://api.funco.co.kr")
-			.allowedHeaders("Content-Type", "Authorization") // Authorization 헤더 허용
-			.allowCredentials(true)
-			.maxAge(3600)
-			.exposedHeaders("*");
-	}
+	// @Override
+	// public void addCorsMappings(CorsRegistry registry) {
+	// 	registry.addMapping("/**")
+	// 		.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+	// 		.allowedOrigins("https://funco.co.kr", "https://api.funco.co.kr")
+	// 		.allowedHeaders("Content-Type", "Authorization") // Authorization 헤더 허용
+	// 		.allowCredentials(true)
+	// 		.maxAge(3600)
+	// 		.exposedHeaders("*");
+	// }
 
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
