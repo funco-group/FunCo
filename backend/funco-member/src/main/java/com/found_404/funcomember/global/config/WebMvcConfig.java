2c4353e475179f3215e829d129ffa31d5140d8e1
이태호 <leetag1163@naver.com>
2024-05-13 13:39:53 +0900

    fix: import 수정

--- backend/funco-member/src/main/java/com/found_404/funcomember/global/config/WebMvcConfig.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/global/config/WebMvcConfig.java
@@ -1,20 +1,16 @@
 package com.found_404.funcomember.global.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.found_404.funcomember.global.util.AuthMemberIdArgumentResolver;
 import com.found_404.funcomember.global.util.OauthServerTypeConverter;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-	private final AuthMemberIdArgumentResolver authMemberIdArgumentResolver;
+	// private final AuthMemberIdArgumentResolver authMemberIdArgumentResolver;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -32,12 +28,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addConverter(new OauthServerTypeConverter());
 	}
 
-	public WebMvcConfig(AuthMemberIdArgumentResolver authMemberIdArgumentResolver) {
-		this.authMemberIdArgumentResolver = authMemberIdArgumentResolver;
-	}
-
-	@Override
-	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(authMemberIdArgumentResolver);
-	}
+	// public WebMvcConfig(AuthMemberIdArgumentResolver authMemberIdArgumentResolver) {
+	// 	this.authMemberIdArgumentResolver = authMemberIdArgumentResolver;
+	// }
+	//
+	// @Override
+	// public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+	// 	resolvers.add(authMemberIdArgumentResolver);
+	// }
 }
