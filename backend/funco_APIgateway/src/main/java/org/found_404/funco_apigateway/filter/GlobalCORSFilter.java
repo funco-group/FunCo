bfe382572df8f2526e0cf7e888bbbc90f72527f1
이태호 <leetag1163@naver.com>
2024-05-13 12:49:31 +0900

    fix: CORS Filter 수정

--- backend/funco_APIgateway/src/main/java/org/found_404/funco_apigateway/filter/GlobalCORSFilter.java
+++ backend/funco_APIgateway/src/main/java/org/found_404/funco_apigateway/filter/GlobalCORSFilter.java
@@ -2,17 +2,14 @@ package org.found_404.funco_apigateway.filter;
 
 import static org.springframework.http.HttpHeaders.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -21,34 +18,35 @@ import reactor.core.publisher.Mono;
 
 @Configuration
 public class GlobalCORSFilter {
-
-	private static final Set<String> ALLOWED_ORIGINS = new HashSet<>(Set.of("https://www.funco.co.kr", "https://funco.co.kr"));
-	private static final String ALLOWED_HEADERS = "X-Requested-With, Authorization, Content-Type";
+	private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type";
 	private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
+	private static final String ALLOWED_ORIGIN = "https://funco.co.kr";
 	private static final String MAX_AGE = "3600";
+
 	private static final String ALLOWED_CREDENTIALS = "true";
 
 	@Bean
 	public WebFilter corsFilter() {
-		return (ServerWebExchange exchange, WebFilterChain chain) -> {
-			ServerHttpRequest request = exchange.getRequest();
-			ServerHttpResponse response = exchange.getResponse();
-			String requestOrigin = request.getHeaders().getOrigin();
 
-			if (ALLOWED_ORIGINS.contains(requestOrigin)) {
-				response.getHeaders().set(ACCESS_CONTROL_ALLOW_ORIGIN, requestOrigin);
-				response.getHeaders().set(ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHODS);
-				response.getHeaders().set(ACCESS_CONTROL_MAX_AGE, MAX_AGE);
-				response.getHeaders().set(ACCESS_CONTROL_ALLOW_HEADERS, ALLOWED_HEADERS);
-				response.getHeaders().set(ACCESS_CONTROL_ALLOW_CREDENTIALS, ALLOWED_CREDENTIALS);
+		return (ServerWebExchange ctx, WebFilterChain chain) -> {
+
+			ServerHttpRequest request = ctx.getRequest();
+
+			if (CorsUtils.isPreFlightRequest(request)) {
+				ServerHttpResponse response = ctx.getResponse();
+				HttpHeaders headers = response.getHeaders();
+				headers.add(ACCESS_CONTROL_ALLOW_ORIGIN, ALLOWED_ORIGIN);
+				headers.add(ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHODS);
+				headers.add(ACCESS_CONTROL_MAX_AGE, MAX_AGE);
+				headers.add(ACCESS_CONTROL_ALLOW_HEADERS, ALLOWED_HEADERS);
+				headers.add(ACCESS_CONTROL_ALLOW_CREDENTIALS, ALLOWED_CREDENTIALS);
+
+				if (request.getMethod() == HttpMethod.OPTIONS) {
+					response.setStatusCode(HttpStatus.OK);
+					return Mono.empty();
+				}
 			}
-
-			if (request.getMethod().equals(HttpMethod.OPTIONS)) {
-				response.setStatusCode(HttpStatus.OK);
-				return Mono.empty();
-			}
-
-			return chain.filter(exchange);
+			return chain.filter(ctx);
 		};
 	}
 }
