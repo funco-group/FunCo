32aab74ec12b72be19ccb0a3df09a3a6a748e08d
황주영 <ghkdwndud7@naver.com>
2024-05-13 15:27:38 +0900

    feat: 상세 페이지 글 내용 구현

--- frontend/src/components/Note/Detail/NotesDetailArticle.tsx
+++ frontend/src/components/Note/Detail/NotesDetailArticle.tsx
@@ -1,10 +1,55 @@
-import DummyNotes from '@/lib/DummyNote'
+'use client'
 
-function NotesDetailArticle() {
-  const detail = DummyNotes
+/* eslint-disable @next/next/no-img-element */
+import DummyNotes from '@/lib/DummyNote'
+import noteParseDate from '@/utils/noteParseDate'
+import dynamic from 'next/dynamic'
+import LikeSVG from '@/../public/icon/like.svg'
+
+const ToastViewer = dynamic(
+  () => import('@/components/Common/ToastUI/ToastViewer'),
+  {
+    ssr: false,
+  },
+)
+
+interface NotesDetailArticleProps {
+  noteId: number
+}
+
+function NotesDetailArticle({ noteId }: NotesDetailArticleProps) {
+  const detail = DummyNotes[noteId - 1]
   return (
     <div className="rounded border border-solid border-deactivatedGray bg-brandWhite p-3">
-      NotesDetailArticle
+      <div className="w-fit rounded bg-brandColor p-1 text-brandWhite">
+        {detail.coinName}
+      </div>
+      <h2>{detail.title}</h2>
+      <div className="flex">
+        <img
+          src={detail.profileImage}
+          className="block h-10 w-10 rounded-full"
+          alt="profile"
+        />
+        <div className="ml-2 flex flex-col justify-center text-xs">
+          <div>{detail.nickname}</div>
+          <div className=" text-brandDarkGray">
+            {noteParseDate(detail.writeDate)}
+          </div>
+        </div>
+      </div>
+      <div className="my-3">
+        <ToastViewer initialValue={detail.content} />
+      </div>
+      <div className="flex justify-center">
+        <div className="rounded border border-solid border-borderGray p-4">
+          {detail.liked ? (
+            <LikeSVG fill="red" />
+          ) : (
+            <LikeSVG fill="transparent" stroke="black" />
+          )}
+        </div>
+      </div>
     </div>
   )
 }
