24835775e424ba1f7af313515ddec61056321ab9
소재열 <devjy0309@gmail.com>
2024-05-14 11:08:08 +0900

    feat: crypto 시세 조회 api

--- backend/funco-trade/src/main/java/com/found_404/funco/trade/controller/CryptoController.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/trade/controller/CryptoController.java
@@ -1,7 +1,15 @@
 package com.found_404.funco.trade.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.found_404.funco.trade.cryptoPrice.CryptoPrice;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,5 +18,11 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/crypto")
 public class CryptoController {
 	// 시세 조회 용
+	private final CryptoPrice cryptoPrice;
+
+	@GetMapping()
+	public ResponseEntity<Map<String, Long>> getCryptoPrice(@RequestParam List<String> tickers) {
+		return ResponseEntity.ok(cryptoPrice.getTickerPriceMap(tickers));
+	}
 
 }
