ef32da247e2941518eb8b6d2718225c207ea0c7c
이태호 <leetag1163@naver.com>
2024-05-14 10:54:42 +0900

    init: statistics-service project

---
+++ backend/funco-statistics/src/main/java/com/found_404/funco/global/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.found_404.funco.global.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.found_404.funco.global.util.AuthMemberIdArgumentResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final AuthMemberIdArgumentResolver authMemberIdArgumentResolver;
+
+	public WebMvcConfig(AuthMemberIdArgumentResolver authMemberIdArgumentResolver) {
+		this.authMemberIdArgumentResolver = authMemberIdArgumentResolver;
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(authMemberIdArgumentResolver);
+	}
+}
