43c6912ed8c286d1e0b92e29a77837669cef5129
이선주 <sunju5402@naver.com>
2024-05-14 13:28:25 +0900

    test: 관심코인 삭제 성공 테스트 코드 구현

--- backend/funco/src/test/java/com/found_404/funco/favoritecoin/FavoriteServiceTest.java
+++ backend/funco/src/test/java/com/found_404/funco/favoritecoin/FavoriteServiceTest.java
@@ -78,4 +78,23 @@ public class FavoriteServiceTest {
 		assertEquals(new HashSet<>(Arrays.asList("KRW-BTC", "KRW-ETH")), new HashSet<>(response.tickers()));
 	}
 
+	@Test
+	@DisplayName("관심코인 삭제 성공")
+	void deleteFavoriteCoinSuccess() {
+		// given
+		Long memberId = 1L;
+		String tickerToDelete = "KRW-BTC";
+		FavoriteCoinInfo info = FavoriteCoinInfo.builder().build();
+		info.createFavorite(tickerToDelete);
+
+		given(valueOperations.get(memberId.toString())).willReturn(info);
+		doNothing().when(favoriteCoinService).updateFavoriteCoinZSet(eq(memberId), any(LocalDateTime.class));
+
+		// when
+		favoriteCoinService.deleteFavoriteCoin(memberId, tickerToDelete);
+
+		// then
+		verify(valueOperations, times(1)).set(eq(memberId.toString()), any(FavoriteCoinInfo.class));
+	}
+
 }
