6838410424139b86df2126569cfc5e7bdbb6c9df
이태호 <leetag1163@naver.com>
2024-05-14 10:54:42 +0900

    init: statistics-service project

---
+++ backend/funco-statistics/src/main/java/com/found_404/funco/global/util/AuthMemberId.java
@@ -0,0 +1,11 @@
+package com.found_404.funco.global.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthMemberId {
+}
