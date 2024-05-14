29025404ac677d0c95e5ee513a221586b403089d
소재열 <devjy0309@gmail.com>
2024-05-14 16:39:11 +0900

    init: msa follow service project 생성 및 의존성 분리해서 이전 서버 오픈

---
+++ backend/funco-follow/src/main/java/com/found_404/funco/follow/domain/repository/FollowRepository.java
@@ -0,0 +1,23 @@
+package com.found_404.funco.follow.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.found_404.funco.follow.domain.Follow;
+
+public interface FollowRepository extends JpaRepository<Follow, Long>, QueryDslFollowRepository {
+	Optional<Follow> findFollowByFollowingMemberIdAndFollowerMemberIdAndSettledFalse(Long followingMemberId,
+		Long followerMemberId);
+
+	@EntityGraph(attributePaths = {"following","follower"})
+	List<Follow> findAllByFollowerMemberIdAndSettled(Long followerMemberId, Boolean settled);
+
+	List<Follow> findAllByFollowerMemberIdAndSettledFalse(Long followerMemberId);
+
+    List<Follow> findAllByFollowerMemberIdAndSettledTrue(Long followerMemberId);
+
+	List<Follow> findAllByFollowingMemberIdAndSettledTrue(Long followingMemberId);
+}
