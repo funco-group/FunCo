8ea762b95eed62756dfd1128014ca8f591757b3d
이태호 <leetag1163@naver.com>
2024-05-14 21:49:23 +0900

    init: Auth Service project init

---
+++ backend/funco-auth/src/main/java/com/found_404/funco/auth/client/MemberServiceClient.java
@@ -0,0 +1,20 @@
+package com.found_404.funco.auth.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.found_404.funco.auth.dto.request.OAuthMemberRequest;
+import com.found_404.funco.auth.dto.response.OAuthMemberResponse;
+
+@FeignClient(name = "member-service")
+public interface MemberServiceClient {
+	@GetMapping(value = "/api/v1/members/auth/{provider}/{oauthId}")
+	OAuthMemberResponse getAuthMember(@PathVariable String provider,
+		@PathVariable String oauthId);
+
+	@PostMapping(value = "/api/v1/members")
+	OAuthMemberResponse addAuthMember(@RequestBody OAuthMemberRequest oauthMemberRequest);
+}
