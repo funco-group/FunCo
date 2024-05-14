7dd64fae1372010117969d4dd1ef65ff4f936dc6
소재열 <devjy0309@gmail.com>
2024-05-14 14:31:05 +0900

    refactor: feign 설정 분리

--- backend/funco-trade/src/main/java/com/found_404/funco/FuncoTradeApplication.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/FuncoTradeApplication.java
@@ -1,13 +1,8 @@
 package com.found_404.funco;
 
-import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
-
-@EnableFeignClients
 @SpringBootApplication
 public class FuncoTradeApplication {
 
@@ -15,8 +10,4 @@ public class FuncoTradeApplication {
 		SpringApplication.run(FuncoTradeApplication.class, args);
 	}
 
-	@Bean
-	public Logger.Level feignLoggerLevel() {
-		return Logger.Level.FULL;
-	}
 }
