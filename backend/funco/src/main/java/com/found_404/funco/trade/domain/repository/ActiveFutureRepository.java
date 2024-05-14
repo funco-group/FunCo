b65d7a62beaaaac72c3d01fb84fcf45f9db68dae
박세웅 <ert4263@gmail.com>
2024-05-14 13:53:14 +0900

    feat: Repository에 Member가 보유중인 선물 거래 내역 조회하는 메서드 작성

--- backend/funco/src/main/java/com/found_404/funco/trade/domain/repository/ActiveFutureRepository.java
+++ backend/funco/src/main/java/com/found_404/funco/trade/domain/repository/ActiveFutureRepository.java
@@ -1,8 +1,14 @@
 package com.found_404.funco.trade.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.found_404.funco.member.domain.Member;
 import com.found_404.funco.trade.domain.ActiveFuture;
 
 public interface ActiveFutureRepository extends JpaRepository<ActiveFuture, Long> {
+
+	List<ActiveFuture> findActiveFutureByMember(Member member);
+
 }
