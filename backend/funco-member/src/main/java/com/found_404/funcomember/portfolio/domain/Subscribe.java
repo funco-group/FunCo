36d0a81c4afd9bf3860a8e002b2057ad6043d3c4
sunju5402 <ssonju5402@gmail.com>
2024-05-15 19:18:15 +0900

    feat: 포트폴리오 entity 생성

---
+++ backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/domain/Subscribe.java
@@ -0,0 +1,58 @@
+package com.found_404.funcomember.portfolio.domain;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.Comment;
+
+import com.found_404.funcomember.global.entity.BaseEntity;
+import com.found_404.funcomember.member.domain.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(uniqueConstraints = {
+	@UniqueConstraint(
+		columnNames = {
+			"from_member_id",
+			"to_member_id"
+		}
+	)
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Subscribe extends BaseEntity {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "from_member_id", nullable = false)
+	private Member fromMember;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "to_member_id", nullable = false)
+	private Member toMember;
+
+	@Comment("주문금액")
+	@Column(nullable = false)
+	private Long orderCash;
+
+	@Comment("구독 만료 일자")
+	@Column(nullable = false)
+	private LocalDateTime expiredAt;
+
+	@Builder
+	public Subscribe(Member fromMember, Member toMember, Long orderCash, LocalDateTime expiredAt) {
+		this.fromMember = fromMember;
+		this.toMember = toMember;
+		this.orderCash = orderCash;
+		this.expiredAt = expiredAt;
+	}
+}
