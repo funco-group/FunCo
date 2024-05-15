82f6f3c587c4b76c333121f4839f869ca1b23457
이선주 <sunju5402@naver.com>
2024-05-16 01:42:48 +0900

    feat: 수익률 및 수익금 조회 로직 구현

--- backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/client/TradeServiceClient.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/client/TradeServiceClient.java
@@ -1,8 +1,14 @@
 package com.found_404.funcomember.feignClient.client;
 
 import com.found_404.funcomember.feignClient.dto.CoinValuationResponse;
+import com.found_404.funcomember.feignClient.dto.ProfitResponse;
+import com.found_404.funcomember.feignClient.dto.ReturnRateResponse;
+import com.found_404.funcomember.portfolio.dto.request.ProfitRequest;
+import com.found_404.funcomember.portfolio.dto.request.ReturnRateRequest;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -17,6 +23,12 @@ public interface TradeServiceClient {
 
     @GetMapping("/api/crypto")
     Map<String, Long> getCryptoPrice(@RequestParam List<String> tickers);
+
+    @GetMapping("/api/trade/portfolio/return-rate")
+    ReturnRateResponse getReturnRate(@RequestBody ReturnRateRequest returnRateRequest);
+
+    @GetMapping("/api/trade/portfolio/profit")
+    ProfitResponse getProfit(@RequestBody ProfitRequest profitRequest);
 
     @GetMapping("/api/v1/hello")
     String hello();
