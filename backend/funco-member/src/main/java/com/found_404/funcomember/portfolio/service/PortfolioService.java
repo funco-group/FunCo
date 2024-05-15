76e5e3da2d8f1d155900081f8f162a1200def30e
이선주 <sunju5402@naver.com>
2024-05-16 01:41:57 +0900

    feat: 서버에서 불러운 데이터 바탕으로 로직 구현

--- backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/service/PortfolioService.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/service/PortfolioService.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.found_404.funcomember.feignClient.dto.FollowerInfoResponse;
+import com.found_404.funcomember.feignClient.service.FollowService;
+import com.found_404.funcomember.feignClient.service.TradeService;
 import com.found_404.funcomember.member.domain.Member;
 import com.found_404.funcomember.member.domain.repository.MemberRepository;
 import com.found_404.funcomember.member.domain.type.PortfolioStatusType;
@@ -13,8 +16,10 @@ import com.found_404.funcomember.member.exception.MemberErrorCode;
 import com.found_404.funcomember.member.exception.MemberException;
 import com.found_404.funcomember.portfolio.domain.Subscribe;
 import com.found_404.funcomember.portfolio.domain.repository.SubscribeRepository;
-import com.found_404.funcomember.portfolio.dto.FollowerInfo;
+import com.found_404.funcomember.portfolio.dto.request.FollowerProfitRequest;
 import com.found_404.funcomember.portfolio.dto.request.PortfolioStatusRequest;
+import com.found_404.funcomember.portfolio.dto.request.ProfitRequest;
+import com.found_404.funcomember.portfolio.dto.request.ReturnRateRequest;
 import com.found_404.funcomember.portfolio.dto.request.SubscribeRequest;
 import com.found_404.funcomember.portfolio.exception.PortfolioErrorCode;
 import com.found_404.funcomember.portfolio.exception.PortfolioException;
@@ -26,6 +31,8 @@ import lombok.RequiredArgsConstructor;
 public class PortfolioService {
 	private final MemberRepository memberRepository;
 	private final SubscribeRepository subscribeRepository;
+	private final FollowService followService;
+	private final TradeService tradeService;
 
 	@Transactional
 	public void updatePortfolioStatus(Long memberId, PortfolioStatusRequest portfolioStatusRequest) {
@@ -49,19 +56,22 @@ public class PortfolioService {
 			throw new PortfolioException(PortfolioErrorCode.INSUFFICIENT_CASH);
 		}
 
-		// double ratio = divide(portfolioPrice, seller.getCash(), ScaleType.RETURN_RATE_SCALE);
-		//
-		// subscriber.decreaseCash(portfolioPrice);
-		// seller.increaseCashWithoutCommission(portfolioPrice);
+		Double ratio = tradeService.getReturnRate(ReturnRateRequest.builder()
+			.portfolioPrice(portfolioPrice)
+			.cash(seller.getCash())
+			.build());
+
+		subscriber.decreaseCash(portfolioPrice);
+		seller.increaseCashWithoutCommission(portfolioPrice);
 
 		// 구독
 		subscribe(subscriber, seller);
 
 		// seller의 팔로워들 동기화(더해줌)
-		//synchronizeFollowers(seller.getId(), ratio);
+		synchronizeFollowers(seller.getId(), ratio);
 
 		// subscriber의 팔로워들 동기화(빼줌)
-		//synchronizeFollowers(subscriber.getId(), -ratio);
+		synchronizeFollowers(subscriber.getId(), -ratio);
 	}
 
 	private Member findByMemberId(Long memberId) {
@@ -70,13 +80,20 @@ public class PortfolioService {
 	}
 
 	public void synchronizeFollowers(Long followingId, double ratio) {
-		// List<FollowerInfo> followerInfos = subscribeRepository.findFollowInfoByFollowingId(followingId);
-		// followerInfos.forEach(
-		// 	info -> {
-		// 		long cash = info.cash() + (long)multiple(info.cash(), ratio, ScaleType.CASH_SCALE);
-		// 		subscribeRepository.updateFollower(followingId, info.followerId(), cash);
-		// 	}
-		// );
+		List<FollowerInfoResponse> followerInfos = followService.getFollowerInfos(followingId);
+		followerInfos.forEach(
+			info -> {
+				long cash = info.cash() + tradeService.getProfit(ProfitRequest.builder()
+					.cash(info.cash())
+					.ratio(ratio)
+					.build());
+				followService.modifyFollower(followingId, FollowerProfitRequest
+					.builder()
+					.followerId(info.followerId())
+					.cash(cash)
+					.build());
+			}
+		);
 	}
 
 	private void subscribe(Member subscriber, Member seller) {
