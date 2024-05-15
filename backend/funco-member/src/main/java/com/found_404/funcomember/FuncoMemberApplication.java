22ac0b56ec24e912b490f63b29000a3d45f07efb
이선주 <sunju5402@naver.com>
2024-05-16 01:30:39 +0900

    feat: 스케줄링 적용 및 환경변수 파일 스캔 설정

--- backend/funco-member/src/main/java/com/found_404/funcomember/FuncoMemberApplication.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/FuncoMemberApplication.java
@@ -2,8 +2,12 @@ package com.found_404.funcomember;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@ConfigurationPropertiesScan
 public class FuncoMemberApplication {
 
 	public static void main(String[] args) {
