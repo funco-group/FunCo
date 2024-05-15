2638ae9b23b96ff7638030483b8addd20b04b792
devjy39 <devjy0309@gmail.com>
2024-05-15 17:16:37 +0900

    feat: openfeign 의존성 추가 및 설정

---
+++ backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/config/FeignClientConfig.java
@@ -0,0 +1,17 @@
+package com.found_404.funcomember.feignClient.config;
+
+import feign.Logger;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.found_404.funcomember") // main 패키지
+public class FeignClientConfig {
+
+	@Bean
+	public Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
+
+}
