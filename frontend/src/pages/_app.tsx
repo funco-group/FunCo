ea315350ea048693e73ea570897c58991ba4fc7e
sohy19 <sohy19@hufs.ac.kr>
2024-05-15 22:44:39 +0900

    fix: global font 설정

--- frontend/src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '@/app/globals.css';  // 전역 CSS 파일을 임포트합니다.
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
