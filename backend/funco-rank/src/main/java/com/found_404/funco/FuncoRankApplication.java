44e967848a5b929d456d2b40e71775e560462d82
이태호 <leetag1163@naver.com>
2024-05-15 23:39:03 +0900

    fix: trade client api 경로 수정

--- backend/funco-rank/src/main/java/com/found_404/funco/FuncoRankApplication.java
+++ backend/funco-rank/src/main/java/com/found_404/funco/FuncoRankApplication.java
@@ -2,7 +2,9 @@ package com.found_404.funco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class FuncoRankApplication {
 
