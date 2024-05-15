8bbd06348423583f52357614e3765a14ed5f4797
sunju5402 <ssonju5402@gmail.com>
2024-05-16 01:38:54 +0900

    fix: 불필요한 custom repository 코드 제거

--- backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/domain/repository/SubscribeRepository.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/domain/repository/SubscribeRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.found_404.funcomember.portfolio.domain.Subscribe;
 
-public interface SubscribeRepository extends JpaRepository<Subscribe, Long>, SubscribeCustomRepository {
+public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
 }
