0bc2a67b0f9b863ab912cf1a56a2383e67cb2455
이태호 <leetag1163@naver.com>
2024-05-15 23:00:01 +0900

    feat: 멤버 별 보유 코인 조회 API 구현

--- backend/funco-trade/src/main/java/com/found_404/funco/trade/domain/repository/QueryDslHoldingCoinRepository.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/trade/domain/repository/QueryDslHoldingCoinRepository.java
@@ -2,6 +2,10 @@ package com.found_404.funco.trade.domain.repository;
 
 import java.util.List;
 
+import com.found_404.funco.trade.dto.HoldingCoinInfo;
+
 public interface QueryDslHoldingCoinRepository {
 	List<String> findHoldingCoin();
+
+	List<HoldingCoinInfo> findHoldingCoinInfoList();
 }
--- backend/funco-trade/src/main/java/com/found_404/funco/trade/domain/repository/impl/QueryDslHoldingCoinRepositoryImpl.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/trade/domain/repository/impl/QueryDslHoldingCoinRepositoryImpl.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.found_404.funco.trade.domain.repository.QueryDslHoldingCoinRepository;
+import com.found_404.funco.trade.dto.HoldingCoinInfo;
+import com.found_404.funco.trade.dto.QHoldingCoinInfo;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,16 @@ public class QueryDslHoldingCoinRepositoryImpl implements QueryDslHoldingCoinRep
 			)
 			.from(holdingCoin)
 			.distinct()
+			.fetch();
+	}
+
+	@Override
+	public List<HoldingCoinInfo> findHoldingCoinInfoList() {
+		return jpaQueryFactory.select(
+				new QHoldingCoinInfo(holdingCoin.memberId, holdingCoin.ticker, holdingCoin.volume)
+			)
+			.from(holdingCoin)
+			.groupBy(holdingCoin.memberId, holdingCoin.ticker)
 			.fetch();
 	}
 }
