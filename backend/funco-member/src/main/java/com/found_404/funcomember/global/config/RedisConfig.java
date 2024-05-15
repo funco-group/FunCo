2708264a44f0e92f1e30ffd2e9ee5af96afba7ac
sunju5402 <ssonju5402@gmail.com>
2024-05-15 20:42:08 +0900

    feat:  관심코인 redis template 설정

--- backend/funco-member/src/main/java/com/found_404/funcomember/global/config/RedisConfig.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/global/config/RedisConfig.java
@@ -1,6 +1,9 @@
 package com.found_404.funcomember.global.config;
 
+import static com.found_404.funcomember.global.exception.RedisErrorCode.*;
 import static com.found_404.funcomember.global.type.RedisDatabaseType.*;
+
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,8 +21,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import com.found_404.funcomember.favoritecoin.dto.FavoriteCoinInfo;
+import com.found_404.funcomember.global.exception.RedisException;
 import com.found_404.funcomember.member.dto.RankDto;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 @EnableTransactionManagement
 public class RedisConfig {
@@ -46,6 +54,35 @@ public class RedisConfig {
 		return lettuceConnectionFactory;
 	}
 
+	// 관심있는 코인 템플릿
+	@Bean
+	public RedisTemplate<String, Object> favoriteCoinRedisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(createLettuceConnectionFactory(FAVORITE_COIN.ordinal()));
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		// Value 직렬화를 위한 ObjectMapper 설정
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.registerModule(new Jdk8Module());
+		objectMapper.registerModule(new ParameterNamesModule());
+		// Value 직렬화 설정
+		Jackson2JsonRedisSerializer<FavoriteCoinInfo> jackson2JsonRedisSerializer =
+			new Jackson2JsonRedisSerializer<>(objectMapper, FavoriteCoinInfo.class);
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
+		return redisTemplate;
+	}
+
+	// 관심코인 zset 템플릿
+	@Bean
+	public RedisTemplate<String, Object> favoriteCoinZSetRedisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(createLettuceConnectionFactory(FAVORITE_COIN.ordinal()));
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer()); // ZSet 값에 대한 직렬화
+		return redisTemplate;
+	}
+
 	// 랭킹 zset 템플릿
 	@Bean
 	public RedisTemplate<String, Object> rankZSetRedisTemplate() {
@@ -61,6 +98,7 @@ public class RedisConfig {
 		Jackson2JsonRedisSerializer<RankDto> jackson2JsonRedisSerializer =
 			new Jackson2JsonRedisSerializer<>(objectMapper, RankDto.class);
 		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
 		return redisTemplate;
 	}
 
