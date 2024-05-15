9f864c443db0d44f433d713ba0c43eb01c44627a
이태호 <leetag1163@naver.com>
2024-05-16 00:04:45 +0900

    fix: follow client api 경로 수정

--- backend/funco-rank/src/main/java/com/found_404/funco/feignClient/client/FollowServiceClient.java
+++ backend/funco-rank/src/main/java/com/found_404/funco/feignClient/client/FollowServiceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(name = "follow-service")
 public interface FollowServiceClient {
 
-	@GetMapping(value = "/api/v1/batch/follows/investment")
+	@GetMapping(value = "/api/v1/batch/follower/investment")
 	Map<Long, Long> getFollowerInvestmentList();
 
 	@GetMapping("/api/v1/batch/following/investment")
