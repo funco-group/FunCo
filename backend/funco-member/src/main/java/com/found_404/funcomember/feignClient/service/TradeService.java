a4facd8fc52c7b1c40c9c0ec136b63422b830e89
sunju5402 <ssonju5402@gmail.com>
2024-05-16 01:42:48 +0900

    feat: 수익률 및 수익금 조회 로직 구현

--- backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/service/TradeService.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/service/TradeService.java
@@ -2,11 +2,17 @@ package com.found_404.funcomember.feignClient.service;
 
 import com.found_404.funcomember.feignClient.client.TradeServiceClient;
 import com.found_404.funcomember.feignClient.dto.CoinValuationResponse;
+import com.found_404.funcomember.feignClient.dto.ProfitResponse;
+import com.found_404.funcomember.feignClient.dto.ReturnRateResponse;
 import com.found_404.funcomember.member.exception.MemberException;
+import com.found_404.funcomember.portfolio.dto.request.ProfitRequest;
+import com.found_404.funcomember.portfolio.dto.request.ReturnRateRequest;
+
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Map;
@@ -34,6 +40,24 @@ public class TradeService {
 			return tradeServiceClient.getCryptoPrice(tickers);
 		} catch (FeignException e) {
 			log.error("{} Crypto price error : {}", SERVER_NAME, e.getMessage());
+			throw new MemberException(TRADE_SERVER_ERROR);
+		}
+	}
+
+	public Double getReturnRate(ReturnRateRequest returnRateRequest) {
+		try {
+			return tradeServiceClient.getReturnRate(returnRateRequest).ratio();
+		} catch (FeignException e) {
+			log.error("{} getReturnRate error : {}", SERVER_NAME, e.getMessage());
+			throw new MemberException(TRADE_SERVER_ERROR);
+		}
+	}
+
+	public Long getProfit(ProfitRequest profitRequest) {
+		try {
+			return tradeServiceClient.getProfit(profitRequest).profit();
+		} catch (FeignException e) {
+			log.error("{} getProfit error : {}", SERVER_NAME, e.getMessage());
 			throw new MemberException(TRADE_SERVER_ERROR);
 		}
 	}
