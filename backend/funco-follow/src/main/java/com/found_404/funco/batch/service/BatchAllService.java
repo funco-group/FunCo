b92903e3c627e36e9416304b572ccb523e74fd4a
이태호 <leetag1163@naver.com>
2024-05-15 23:05:46 +0900

    feat: 멤버 별 총 따라오는 투자금 조회 API 구현

---
+++ backend/funco-follow/src/main/java/com/found_404/funco/batch/service/BatchAllService.java
@@ -0,0 +1,19 @@
+package com.found_404.funco.batch.service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.found_404.funco.follow.domain.repository.FollowRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BatchAllService {
+	private final FollowRepository followRepository;
+
+	public Map<Long, Long> readFollowerInvestmentList() {
+		return followRepository.findFollowerInvestmentList();
+	}
+}
