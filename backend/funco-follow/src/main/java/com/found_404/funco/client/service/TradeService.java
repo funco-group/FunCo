3ccc3b3d4f7996630a4390d7c7bfa9cc96a318fd
devjy39 <devjy0309@gmail.com>
2024-05-15 15:07:33 +0900

    feat: 팔로우 등록, 정산 API 분리

--- backend/funco-follow/src/main/java/com/found_404/funco/client/service/TradeService.java
+++ backend/funco-follow/src/main/java/com/found_404/funco/client/service/TradeService.java
@@ -12,17 +12,30 @@ import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 @Slf4j
 public class TradeService {
 	private final TradeServiceClient tradeServiceClient;
+	private final String SERVER_NAME = "[trade-service]";
 
 	public CoinValuationResponse getCoinValuations(Long memberId) {
 		try {
 			return tradeServiceClient.getCoinValuations(memberId);
 		} catch (FeignException e) {
-			log.error("trade service updateMemberCash error : {}", e.getMessage());
+			log.error("{} updateMemberCash error : {}", SERVER_NAME, e.getMessage());
+			throw new FollowException(TRADE_SERVER_ERROR);
+		}
+	}
+
+	public Map<String, Long> getCryptoPrice(List<String> tickers) {
+		try {
+			return tradeServiceClient.getCryptoPrice(tickers);
+		} catch (FeignException e) {
+			log.error("{} Crypto price error : {}", SERVER_NAME, e.getMessage());
 			throw new FollowException(TRADE_SERVER_ERROR);
 		}
 	}
--- backend/funco-follow/src/main/java/com/found_404/funco/follow/service/FollowService.java
+++ backend/funco-follow/src/main/java/com/found_404/funco/follow/service/FollowService.java
@@ -5,11 +5,7 @@ import static com.found_404.funco.global.util.DecimalCalculator.ScaleType.*;
 import static com.found_404.funco.global.util.DecimalCalculator.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import com.found_404.funco.client.dto.CoinValuation;
@@ -25,6 +21,7 @@ import com.found_404.funco.follow.domain.repository.FollowingCoinRepository;
 import com.found_404.funco.follow.domain.type.TradeType;
 import com.found_404.funco.follow.dto.FollowTradeDto;
 import com.found_404.funco.follow.dto.HoldingCoinsDto;
+import com.found_404.funco.follow.dto.RatioPrice;
 import com.found_404.funco.follow.dto.SliceFollowingInfo;
 import com.found_404.funco.follow.dto.request.FollowingRequest;
 import com.found_404.funco.follow.dto.response.FollowerListResponse;
@@ -54,104 +51,134 @@ public class FollowService {
 
 	// private final NotificationService notificationService;
 
+	/*
+	*	부모 = 팔로우 당하는 사람, 팔로잉
+	*	자식 = 팔로우 하는 사람, 팔로워
+	* */
 	@Transactional
-	public void createFollow(FollowingRequest request, Long memberId) {
+	public void createFollow(FollowingRequest request, Long followerMemberId) {
 
-		// 팔로우 하려는 대상이 본인이라면 예외
-		if (request.memberId().equals(memberId)) {
-			throw new FollowException(FOLLOW_SELF_ERROR);
-		}
+		// 팔로우 하려는 대상이 본인이라면, 이미 팔로우 되어있다면 예외 예외
+		checkSelfFollow(request, followerMemberId);
+		checkDuplicatedFollow(request.memberId(), followerMemberId);
 
 		// 부모 팔로우 멤버
 		Long followingMemberId = request.memberId();
 
-		// 자식 팔로우 멤버
-		Long followerMemberId = request.memberId();
-
-		// 팔로우 되어있다면 예외
-		Optional<Follow> selectFollow = followRepository.findFollowByFollowingMemberIdAndFollowerMemberIdAndSettledFalse(
-			followingMemberId, followerMemberId);
-		if (selectFollow.isPresent()) {
-			throw new FollowException(FOLLOW_DUPLICATED_ERROR);
-		}
-
 		// 초기 투자금
 		Long investment = request.investment();
 
-		// 부모 팔로워 가용 현금
+		// [API select] 부모 가용 현금
 		Long followingCash = memberService.getMemberCash(followingMemberId);
 
-		CoinValuationResponse coinValuationResponse = tradeService.getCoinValuations(memberId);
+		// [API select] 부모 투자정보
+		CoinValuationResponse coinValuationResponse = tradeService.getCoinValuations(followerMemberId);
 
-		Long followingFollowSum = followRepository.findAllByFollowerMemberId(followingMemberId)
-			.stream().map(Follow::getInvestment)
-			.reduce(0L, Long::sum);
+		// 부모의 팔로우 투자금 총액
+		Long followingTotalFollowInvestment = getTotalFollowInvestment(followingMemberId);
 
-		// 부모 총 보유 자산 = 부모의 거래자산 총합 + 부모의 가용 현금 + 부모 팔로우 총 합
-		long followingAsset = coinValuationResponse.totalTradeAsset() + followingCash + followingFollowSum;
+		// 부모 총 보유 자산 = 부모의 거래자산 총합 + 부모의 가용 현금 + 부모의 팔로우 투자금 합
+		long followingAsset = coinValuationResponse.totalTradeAsset() + followingCash + followingTotalFollowInvestment;
 
 		// 부모의 보유 자산의 비율
-		Map<String, Double> followingAssetRatio = coinValuationResponse.coinValuations()
-			.stream()
-			.collect(Collectors.toMap(CoinValuation::ticker,
-				coinValuation -> divide(coinValuation.valuation(), followingAsset, NORMAL_SCALE)));
-
-		// 팔로우의 가용 현금 = 투자금 - 산 금액
-		//Long followerCash = (long)multiple(investment, divide(followingCash, followingAsset, NORMAL_SCALE), CASH_SCALE);
+		Map<String, RatioPrice> followingAssetRatio = coinValuationResponse.coinValuations()
+				.stream()
+				.collect(Collectors.toMap(CoinValuation::ticker,
+						coinValuation -> new RatioPrice(divide(coinValuation.valuation(), followingAsset, NORMAL_SCALE), coinValuation.price())));
 
 		// 팔로우 생성
 		Follow follow = Follow.builder()
-			.followingMemberId(followingMemberId)
-			.followerMemberId(followerMemberId)
-			.investment(investment)
-			.cash(followerCash)
-			.settled(Boolean.FALSE)
-			.build();
+				.followingMemberId(followingMemberId)
+				.followerMemberId(followerMemberId)
+				.investment(investment)
+				.cash(investment) // 처음엔 초기 투자금
+				.settled(Boolean.FALSE)
+				.build();
 
-		// 팔로잉 코인, 거래 내역 생성
 		/*
 		 * 팔로잉 코인 갯수 = (초기 투자금 * 부모의 전체 자산에 대해 해당 코인이 차지하는 비율) / 해당 코인의 현재 시세
 		 * 주문 가격 = 초기 투자금 * 부모의 해당 코인의 전체 자산에 대한 비율 => 부모의 코인 비율만큼 사는 것이기 때문
 		 * */
-		Map<FollowingCoin, FollowTrade> followingCoinFollowTradeMap = followingAssetRatio.entrySet().stream()
-			.collect(Collectors.toMap(entry -> FollowingCoin.builder()
-					.follow(follow)
-					.ticker(entry.getKey())
-					.volume(divide(multiple(investment, entry.getValue(), NORMAL_SCALE),
-						followingCryptoPriceMap.get(entry.getKey()), VOLUME_SCALE))
-					.averagePrice(followingCryptoPriceMap.get(entry.getKey()))
-					.build(),
-				entry -> FollowTrade.builder()
-					.follow(follow)
-					.ticker(entry.getKey())
-					.tradeType(TradeType.BUY)
-					.volume(
-						divide(multiple(investment, entry.getValue(), NORMAL_SCALE),
-							followingCryptoPriceMap.get(entry.getKey()), VOLUME_SCALE))
-					.orderCash((long)multiple(investment, entry.getValue(), CASH_SCALE))
-					.price(followingCryptoPriceMap.get(entry.getKey()))
-					.build()));
+		List<FollowingCoin> followingCoins = new ArrayList<>();
+		List<FollowTrade> followTrades = new ArrayList<>();
+
+		followingAssetRatio.forEach((ticker, value) -> {
+            FollowingCoin newCoin = FollowingCoin.builder()
+                    .follow(follow)
+                    .ticker(ticker)
+                    .volume(divide(
+                            	multiple(investment, value.ratio(), NORMAL_SCALE),
+							value.price(), VOLUME_SCALE))
+                    .averagePrice(value.price())
+                    .build();
+            followingCoins.add(newCoin);
+            followTrades.add(getFollowTrade(follow, newCoin));
+        });
 
 		// 엔티티 insert
 		followRepository.save(follow);
-		followingCoinRepository.saveAll(followingCoinFollowTradeMap.keySet());
-		followTradeRepository.saveAll(followingCoinFollowTradeMap.values());
+		followingCoinRepository.saveAll(followingCoins);
+		followTradeRepository.saveAll(followTrades);
 
+		// 산 만큼 팔로우 현금 차감
+		follow.decreaseCash(followTrades.stream()
+				.map(FollowTrade::getOrderCash)
+				.reduce(0L, Long::sum));
 
-		// 팔로워 초기 투자금 차감
+		// [API update] 팔로워 자산 차감
 		memberService.updateMemberCash(followerMemberId, -investment);
 
 		// 알림
+		notification();
+	}
+
+	private Long getTotalFollowInvestment(Long followingMemberId) {
+		return followRepository.findAllByFollowerMemberId(followingMemberId)
+				.stream().map(Follow::getInvestment)
+				.reduce(0L, Long::sum);
+	}
+
+	private void checkSelfFollow(FollowingRequest request, Long followerMemberId) {
+		if (Objects.equals(request.memberId(), followerMemberId)) {
+			throw new FollowException(FOLLOW_SELF_ERROR);
+		}
+	}
+
+	private void checkDuplicatedFollow(Long followingMemberId, Long followerMemberId) {
+		followRepository.findFollowByFollowingMemberIdAndFollowerMemberIdAndSettledFalse(
+				followingMemberId, followerMemberId).ifPresent(follow -> {throw new FollowException(FOLLOW_DUPLICATED_ERROR);});
+	}
+
+	private void notification() {
+		/* 팔로우 */
 		// StringBuilder message = new StringBuilder();
 		// message.append(followerMember.getNickname()).append("님에게 ")
 		// 		.append(String.format("%,d", investment)).append("원을 투자 받으셨습니다.");
 		// notificationService.sendNotification(followingMemberId, NotificationType.FOLLOW, message.toString());
+
+		/* 정산 */
+		// StringBuilder message = new StringBuilder();
+		// message.append(followerMember.getNickname()).append("님이 투자금액 ")
+		// 		.append(String.format("%,d", follow.getInvestment())).append("원을 정산하셨습니다. ")
+		// 		.append(String.format("%,d", commission)).append("원의 수수료를 받았습니다.");
+		// notificationService.sendNotification(followingMemberId, NotificationType.SETTLE, message.toString());
+	}
+
+	private FollowTrade getFollowTrade(Follow follow, FollowingCoin coin) {
+		return FollowTrade.builder()
+				.follow(follow)
+				.ticker(coin.getTicker())
+				.tradeType(TradeType.BUY)
+				.volume(coin.getVolume())
+				.orderCash((long) multiple(coin.getAveragePrice(), coin.getVolume(), CASH_SCALE))
+				.price(coin.getAveragePrice())
+				.build();
 	}
 
 	@Transactional
 	public void deleteFollow(Long followId) {
 		// 팔로우
-		Follow follow = followRepository.findById(followId).orElseThrow(() -> new FollowException(FOLLOW_NOT_FOUND));
+		Follow follow = getFollow(followId);
 
 		// 부모 팔로우 멤버
 		Long followingMemberId = follow.getFollowingMemberId();
@@ -162,12 +189,11 @@ public class FollowService {
 		// 팔로잉 코인
 		List<FollowingCoin> followingCoins = followingCoinRepository.findFollowingCoinsByFollow(follow);
 
-		// 팔로잉 코인들의 현재 시세
-		// Map<String, Long> followingCryptoPriceMap = cryptoPrice.getTickerPriceMap(followingCoins.stream()
-		// 	.map(FollowingCoin::getTicker)
-		// 	.collect(Collectors.toList()));
-		Map<String, Long> followingCryptoPriceMap = new HashMap<>(); // 삭ㅈ
-
+		// [API select] 팔로잉 코인들의 현재 시세
+		Map<String, Long> cryptoPrice = tradeService.getCryptoPrice(
+				followingCoins.stream()
+				.map(FollowingCoin::getTicker)
+				.toList());
 
 		// 거래 내역
 		List<FollowTrade> followTrades = followingCoins.stream()
@@ -177,62 +203,56 @@ public class FollowService {
 				.tradeType(TradeType.SELL)
 				.volume(followingCoin.getVolume())
 				.orderCash(
-					(long)multiple(followingCryptoPriceMap.get(followingCoin.getTicker()), followingCoin.getVolume(),
-						CASH_SCALE))
-				.price(followingCryptoPriceMap.get(followingCoin.getTicker()))
+					(long)multiple(cryptoPrice.get(followingCoin.getTicker()), followingCoin.getVolume(), CASH_SCALE))
+				.price(cryptoPrice.get(followingCoin.getTicker()))
 				.build())
 			.toList();
 
 		// 팔로잉 코인들 가격
-		long followAsset = followingCoins.stream()
-			.mapToLong(followingCoin ->
-				(long)multiple(followingCryptoPriceMap.get(followingCoin.getTicker()), followingCoin.getVolume(),
-					CASH_SCALE))
-			.sum();
+		long sellProfits = followTrades.stream()
+				.map(FollowTrade::getOrderCash)
+				.reduce(0L, Long::sum);
 
 		// 수익금
-		long proceed = followAsset + follow.getCash();
+		long proceed = sellProfits + follow.getCash();
 
 		// 수익률
-		double returnRate = multiple(PERCENT,
-			divide(proceed - follow.getInvestment(), follow.getInvestment(), NORMAL_SCALE), RETURN_RATE_SCALE);
+		double returnRate = getReturnRate(proceed, follow.getInvestment());
 
 		// 수수료
-		long commission =
-			proceed - follow.getInvestment() > 0 ?
-				(long) multiple(proceed - follow.getInvestment(), FOLLOW_FEE, CASH_SCALE) : 0;
+		long commission = getCommission(proceed, follow.getInvestment());
 
 		// 정산 금액
 		long settlement = proceed - commission;
+		log.info("정산 => follow:{} 수익금:{}, 수수료:{}, 수익률:{}", followId, settlement, commission, returnRate);
 
-		log.info("수익금 : {}", proceed);
-		log.info("수익룰 : {}", returnRate);
-		log.info("수수료 : {}", commission);
-		log.info("정산 금액 : {}", settlement);
-
-		// follow update
-		follow.settleFollow(commission, returnRate, LocalDateTime.now(), settlement);
-
-		//  정산 금액 멤버 재산 반영
-		// follower member update
-		// followerMember.settleCash(settlement);
-		//
-		// // following member update
-		// if (commission > 0) {
-		// 	followingMember.settleCash(commission);
-		// }
+		// update follow is settled
+		follow.settleFollow(commission, returnRate, settlement);
 
 		// 데이터 insert
 		followingCoinRepository.deleteAll(followingCoins);
 		followTradeRepository.saveAll(followTrades);
 
-		// 알림
-		// StringBuilder message = new StringBuilder();
-		// message.append(followerMember.getNickname()).append("님이 투자금액 ")
-		// 		.append(String.format("%,d", follow.getInvestment())).append("원을 정산하셨습니다. ")
-		// 		.append(String.format("%,d", commission)).append("원의 수수료를 받았습니다.");
-		// notificationService.sendNotification(followingMemberId, NotificationType.SETTLE, message.toString());
+		// [API update] 자산 반영
+		memberService.updateMemberCash(follow.getFollowerMemberId(), settlement);
+		if (commission > 0) {
+			memberService.updateMemberCash(followingMemberId, commission);
+		}
 
+		notification();
+	}
+
+	private static long getCommission(long proceed, Long investment) {
+		return Math.max((long) multiple(proceed - investment, FOLLOW_FEE, CASH_SCALE), 0);
+	}
+
+	private static double getReturnRate(long proceed, Long investment) {
+		return multiple(PERCENT,
+				divide(proceed - investment, investment, NORMAL_SCALE), RETURN_RATE_SCALE);
+	}
+
+	private Follow getFollow(Long followId) {
+		return followRepository.findById(followId).orElseThrow(() -> new FollowException(FOLLOW_NOT_FOUND));
 	}
 
 	public FollowingListResponse readFollowingList(Long memberId, Long lastFollowId) {
@@ -246,7 +266,7 @@ public class FollowService {
 		// 	holdingCoins.stream()
 		// 		.map(HoldingCoinsDto::ticker)
 		// 		.collect(Collectors.toList()));
-		Map<String, Long> tickerPriceMap = new HashMap<>(); // ㅅ
+		//Map<String, Long> tickerPriceMap = new HashMap<>(); // ㅅ
 
 		// 유저의 보유 코인들의 현재 총 가격
 		// Long totalAsset = holdingCoins.stream()
