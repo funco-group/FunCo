0153449abddf0328d024dc633b6b60d2fb88f998
devjy39 <devjy0309@gmail.com>
2024-05-15 19:44:00 +0900

    fix: client api 수정

--- backend/funco-follow/src/main/java/com/found_404/funco/feignClient/client/TradeServiceClient.java
+++ backend/funco-follow/src/main/java/com/found_404/funco/feignClient/client/TradeServiceClient.java
@@ -15,13 +15,13 @@ import java.util.Map;
 public interface TradeServiceClient {
 
     // 보유 코인 별 평가금액 리스트 api
-    @GetMapping("/api/v1/trade/coinValuations")
+    @GetMapping("/api/trade/coinValuations")
     CoinValuationResponse getCoinValuations(@RequestParam Long memberId);
 
-    @GetMapping("api/v1/crypto")
+    @GetMapping("/api/crypto")
     Map<String, Long> getCryptoPrice(@RequestParam List<String> tickers);
 
-    @GetMapping("/holding/{ticker}")
+    @GetMapping("/trade/holding/{ticker}")
     HoldingCoinResponse getHoldingCoin(@RequestParam Long memberId, @PathVariable String ticker);
 
     @GetMapping("/api/v1/hello")
