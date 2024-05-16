c9cfbccb8ce2386d91f257ff5671f1315419ce95
이태호 <leetag1163@naver.com>
2024-05-16 16:07:23 +0900

    fix: getMemberCash API 경로 수정

--- backend/funco-follow/src/main/java/com/found_404/funco/feignClient/client/MemberServiceClient.java
+++ backend/funco-follow/src/main/java/com/found_404/funco/feignClient/client/MemberServiceClient.java
@@ -1,26 +1,30 @@
 package com.found_404.funco.feignClient.client;
 
-import com.found_404.funco.feignClient.dto.SimpleMember;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
-
-import com.found_404.funco.feignClient.dto.CashResponse;
-import com.found_404.funco.feignClient.dto.UpdateCash;
-
 import java.util.List;
 
-@FeignClient(name="member-service")
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.found_404.funco.feignClient.dto.CashResponse;
+import com.found_404.funco.feignClient.dto.SimpleMember;
+import com.found_404.funco.feignClient.dto.UpdateCash;
+
+@FeignClient(name = "member-service")
 public interface MemberServiceClient {
 
-    @PatchMapping(value = "/api/v1/members/{memberId}/cash")
-    void updateCash(@PathVariable("memberId") Long memberId, @RequestBody UpdateCash updateCash);
+	@PatchMapping(value = "/api/v1/members/{memberId}/cash")
+	void updateCash(@PathVariable("memberId") Long memberId, @RequestBody UpdateCash updateCash);
 
-    @GetMapping("/{memberId}/cash")
-    CashResponse getMemberCash(@PathVariable Long memberId);
+	@GetMapping("/api/v1/members/{memberId}/cash")
+	CashResponse getMemberCash(@PathVariable Long memberId);
 
-    @GetMapping()
-    List<SimpleMember> getMembers(@RequestParam List<Long> ids);
+	@GetMapping()
+	List<SimpleMember> getMembers(@RequestParam List<Long> ids);
 
-    @GetMapping("/api/v1/hello")
-    String hello();
+	@GetMapping("/api/v1/hello")
+	String hello();
 }
