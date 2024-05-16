4d12ee59acb054b6f1b8a5a668b425d817a48f6e
박세웅 <ert4263@gmail.com>
2024-05-17 08:25:41 +0900

    fix: access token 유효 시간 1시간으로 설정

--- backend/funco/src/main/java/com/found_404/funco/global/security/service/TokenService.java
+++ backend/funco/src/main/java/com/found_404/funco/global/security/service/TokenService.java
@@ -49,7 +49,7 @@ public class TokenService {
 	private final MemberRepository memberRepository;
 	private final RedisTemplate<String, Object> tokenRedisTemplate;
 /*	private final long TOKEN_PERIOD = 12 * 60 * 60 * 1000L; // 12h*/
-	private final long TOKEN_PERIOD = 10 * 1000L;
+	private final long TOKEN_PERIOD = 60 * 60 * 1000L; // 1시간
 	private final long REFRESH_PERIOD = 14 * 24 * 60 * 60 * 1000L; // 14일
 	private final String REDIS_REFRESH_TOKEN_KEY = "refreshToken";
 
