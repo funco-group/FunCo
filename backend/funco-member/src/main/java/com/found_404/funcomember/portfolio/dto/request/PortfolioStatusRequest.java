c43685e354723538a8b0808daabc90e3c3574ab2
jaeyeol <devjy0309@gmail.com>
2024-05-17 18:42:41 +0900

    feat: 포트폴리오 벨리데이션 추가

--- backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/dto/request/PortfolioStatusRequest.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/dto/request/PortfolioStatusRequest.java
@@ -1,10 +1,14 @@
 package com.found_404.funcomember.portfolio.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record PortfolioStatusRequest(
-	String portfolioStatus,
-	Long portfolioPrice
+		@NotBlank
+		String portfolioStatus,
+		@NotNull
+		Long portfolioPrice
 ) {
 }
--- backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/dto/request/SubscribeRequest.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/dto/request/SubscribeRequest.java
@@ -1,9 +1,11 @@
 package com.found_404.funcomember.portfolio.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record SubscribeRequest(
-	Long sellerId
+		@NotNull
+		Long sellerId
 ) {
 }
