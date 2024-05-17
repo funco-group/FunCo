7de02585aa391873d1e4ac22a57facfb829b2758
이선주 <sunju5402@naver.com>
2024-05-17 20:38:51 +0900

    fix: 특정 사용자 랭킹 조회 url 변경

--- backend/funco-rank/src/main/java/com/found_404/funco/rank/controller/RankController.java
+++ backend/funco-rank/src/main/java/com/found_404/funco/rank/controller/RankController.java
@@ -6,6 +6,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +31,8 @@ public class RankController {
 		return ResponseEntity.ok(rankService.readRanking(type, pageable));
 	}
 
-	@GetMapping
-	public ResponseEntity<RankingResponse> getRankingInfo(@RequestParam String type, @RequestParam Long memberId) {
+	@GetMapping("/{memberId}")
+	public ResponseEntity<RankingResponse> getRankingInfo(@RequestParam String type, @PathVariable Long memberId) {
 		return ResponseEntity.ok(rankService.getRankingInfo(type, memberId));
 	}
 
