5093104671c28643b5d060c6d479e8d3d1b94c38
이선주 <sunju5402@naver.com>
2024-05-17 20:26:23 +0900

    feat: 보유 코인 및 최근 거래한 코인 구현

--- backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/client/TradeServiceClient.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/client/TradeServiceClient.java
@@ -1,6 +1,8 @@
 package com.found_404.funcomember.feignClient.client;
 
 import com.found_404.funcomember.feignClient.dto.CoinValuationResponse;
+import com.found_404.funcomember.feignClient.dto.HoldingCoinResponse;
+import com.found_404.funcomember.feignClient.dto.RecentTradedCoin;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,12 @@ public interface TradeServiceClient {
 
     @GetMapping("/api/crypto")
     Map<String, Long> getCryptoPrice(@RequestParam List<String> tickers);
+
+    @GetMapping("/api/trade/holding/coins")
+    List<HoldingCoinResponse> getHoldingCoinInfos(@RequestParam Long memberId);
+
+    @GetMapping("/api/trade/recent-coins")
+    List<RecentTradedCoin> getRecentTradedCoins(@RequestParam Long memberId);
 
     @GetMapping("/api/v1/hello")
     String hello();
