baad571cfc5705b83667dba675b34dd756cc96b3
jaeyeol <devjy0309@gmail.com>
2024-05-17 18:51:53 +0900

    refactor: 포폴 구매 예외 추가

--- backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/exception/PortfolioErrorCode.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/exception/PortfolioErrorCode.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum PortfolioErrorCode {
 	INSUFFICIENT_CASH(HttpStatus.BAD_REQUEST, "가용 자산이 충분하지 않습니다."),
+	NOT_PRIVATE_STATUS(HttpStatus.BAD_REQUEST, "판매 중인 상태가 아닙니다.")
 	;
 
 	private final HttpStatus httpStatus;
--- backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/service/PortfolioService.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/service/PortfolioService.java
@@ -2,6 +2,8 @@ package com.found_404.funcomember.portfolio.service;
 
 import static com.found_404.funcomember.global.util.DecimalCalculator.*;
 import static com.found_404.funcomember.global.util.DecimalCalculator.ScaleType.*;
+import static com.found_404.funcomember.portfolio.exception.PortfolioErrorCode.INSUFFICIENT_CASH;
+import static com.found_404.funcomember.portfolio.exception.PortfolioErrorCode.NOT_PRIVATE_STATUS;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,7 +23,6 @@ import com.found_404.funcomember.portfolio.domain.repository.SubscribeRepository
 import com.found_404.funcomember.portfolio.dto.request.FollowerProfitRequest;
 import com.found_404.funcomember.portfolio.dto.request.PortfolioStatusRequest;
 import com.found_404.funcomember.portfolio.dto.request.SubscribeRequest;
-import com.found_404.funcomember.portfolio.exception.PortfolioErrorCode;
 import com.found_404.funcomember.portfolio.exception.PortfolioException;
 
 import lombok.RequiredArgsConstructor;
@@ -49,13 +50,13 @@ public class PortfolioService {
 		Member subscriber = findByMemberId(memberId);
 		Member seller = findByMemberId(subscribeRequest.sellerId());
 
+		checkPortfolioStatus(seller);
+
 		// 포트폴리오 가격 확인
 		Long portfolioPrice = seller.getPortfolioPrice();
 
 		// subscirber의 가용현금으로 seller의 포트폴리오 살 수 없다면 에러 발생
-		if (subscriber.getCash() < portfolioPrice) {
-			throw new PortfolioException(PortfolioErrorCode.INSUFFICIENT_CASH);
-		}
+		checkAbailable(subscriber, portfolioPrice);
 
 		double ratio = divide(portfolioPrice, seller.getCash(), RETURN_RATE_SCALE);
 
@@ -69,6 +70,18 @@ public class PortfolioService {
 
 		// subscriber의 팔로워들 동기화(빼줌)
 		synchronizeFollowers(subscriber.getId(), -ratio);
+	}
+
+	private static void checkAbailable(Member subscriber, Long portfolioPrice) {
+		if (subscriber.getCash() < portfolioPrice) {
+			throw new PortfolioException(INSUFFICIENT_CASH);
+		}
+	}
+
+	private static void checkPortfolioStatus(Member seller) {
+		if (seller.getPortfolioStatus().equals(PortfolioStatusType.PUBLIC)) {
+			throw new PortfolioException(NOT_PRIVATE_STATUS);
+		}
 	}
 
 	private Member findByMemberId(Long memberId) {
