12d696df8f9f287273d74fbcde867a16c94d157a
sunju5402 <ssonju5402@gmail.com>
2024-05-17 20:26:23 +0900

    feat: 보유 코인 및 최근 거래한 코인 구현

--- backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/service/TradeService.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/service/TradeService.java
@@ -1,18 +1,21 @@
 package com.found_404.funcomember.feignClient.service;
 
+import static com.found_404.funcomember.member.exception.MemberErrorCode.*;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
 import com.found_404.funcomember.feignClient.client.TradeServiceClient;
 import com.found_404.funcomember.feignClient.dto.CoinValuationResponse;
+import com.found_404.funcomember.feignClient.dto.HoldingCoinResponse;
+import com.found_404.funcomember.feignClient.dto.RecentTradedCoin;
 import com.found_404.funcomember.member.exception.MemberException;
 
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
-
-import static com.found_404.funcomember.member.exception.MemberErrorCode.TRADE_SERVER_ERROR;
 
 @RequiredArgsConstructor
 @Service
@@ -35,6 +38,24 @@ public class TradeService {
 			return tradeServiceClient.getCryptoPrice(tickers);
 		} catch (FeignException e) {
 			log.error("{} Crypto price error : {}", SERVER_NAME, e.getMessage());
+			throw new MemberException(TRADE_SERVER_ERROR);
+		}
+	}
+
+	public List<HoldingCoinResponse> getHoldingCoinInfos(Long memberId) {
+		try {
+			return tradeServiceClient.getHoldingCoinInfos(memberId);
+		} catch (FeignException e) {
+			log.error("{} get holdingCoins error : {}", SERVER_NAME, e.getMessage());
+			throw new MemberException(TRADE_SERVER_ERROR);
+		}
+	}
+
+	public List<RecentTradedCoin> getRecentTradedCoins(Long memberId) {
+		try {
+			return tradeServiceClient.getRecentTradedCoins(memberId);
+		} catch (FeignException e) {
+			log.error("{} get recentTradedCoins error : {}", SERVER_NAME, e.getMessage());
 			throw new MemberException(TRADE_SERVER_ERROR);
 		}
 	}
