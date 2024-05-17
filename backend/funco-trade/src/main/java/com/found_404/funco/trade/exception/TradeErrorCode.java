e8c93de97ce657da0dd903ff2fe9cb63fb1a7344
devjy39 <devjy0309@gmail.com>
2024-05-18 02:41:35 +0900

    fix: 선물 조회 api 없을 시 응답 400 -> 204로 변경

--- backend/funco-trade/src/main/java/com/found_404/funco/trade/exception/TradeErrorCode.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/trade/exception/TradeErrorCode.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 public enum TradeErrorCode {
     INSUFFICIENT_ASSET(HttpStatus.BAD_REQUEST, "자산이 충분하지 않습니다."),
     PRICE_CONNECTION_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "시세 조회에 실패했습니다."),
-    NOT_FOUND_TRADE(HttpStatus.NOT_FOUND, "해당 거래를 찾을 수 없습니다."),
+    NOT_FOUND_TRADE(HttpStatus.NO_CONTENT, "해당 거래를 찾을 수 없습니다."),
     TRADE_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "해당 권한이 없습니다."),
     ALREADY_FUTURES_TRADE(HttpStatus.BAD_REQUEST, " 이미 해당 선물 거래를 진행중입니다."),
 
