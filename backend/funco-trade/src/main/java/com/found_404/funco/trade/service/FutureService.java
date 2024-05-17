e45187d41d71b189f4cc79863532920a7fbeefea
jaeyeol <devjy0309@gmail.com>
2024-05-17 17:42:09 +0900

    feat: 선물 정산 api 개발

--- backend/funco-trade/src/main/java/com/found_404/funco/trade/service/FutureService.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/trade/service/FutureService.java
@@ -1,15 +1,19 @@
 package com.found_404.funco.trade.service;
 
+import com.found_404.funco.crypto.cryptoPrice.CryptoPrice;
+import com.found_404.funco.feignClient.service.MemberService;
+import com.found_404.funco.trade.domain.ActiveFuture;
+import com.found_404.funco.trade.domain.FutureTrade;
 import com.found_404.funco.trade.domain.repository.ActiveFutureRepository;
 import com.found_404.funco.trade.domain.repository.FutureTradeRepository;
 import com.found_404.funco.trade.dto.FuturesDto;
 import com.found_404.funco.trade.dto.request.RequestBuyFutures;
 import com.found_404.funco.trade.dto.ActiveFutureDto;
-import com.found_404.funco.trade.exception.TradeErrorCode;
 import com.found_404.funco.trade.exception.TradeException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,6 +25,9 @@ import static com.found_404.funco.trade.exception.TradeErrorCode.NOT_FOUND_TRADE
 public class FutureService {
     private final ActiveFutureRepository activeFutureRepository;
     private final FutureTradeRepository futureTradeRepository;
+    private final CryptoPrice cryptoPrice;
+
+    private final MemberService memberService;
 
 
     public void buyFuturesLong(Long memberId, RequestBuyFutures requestBuyFutures) {
@@ -31,8 +38,22 @@ public class FutureService {
 
     }
 
-    public void settlement(Long memberId, String futureId) {
+    @Transactional
+    public void settlement(Long memberId, Long futureId) {
+        ActiveFuture activeFuture = activeFutureRepository.findById(futureId)
+                .orElseThrow(() -> new TradeException(NOT_FOUND_TRADE));
 
+        long price = cryptoPrice.getTickerPrice(activeFuture.getTicker());
+
+        // 투입금 + ( 수익(+-) * 레버리지 )
+        long result = (price - activeFuture.getPrice()) * activeFuture.getLeverage();
+        long settlement = activeFuture.getOrderCash() + result;
+
+        activeFutureRepository.delete(activeFuture);
+        futureTradeRepository.save(FutureTrade.fromActiveFutures(activeFuture, settlement));
+
+        // 멤버 자산 증가
+        memberService.updateMemberCash(memberId, settlement);
     }
 
     public ActiveFutureDto getActiveFutures(Long memberId, String ticker) {
