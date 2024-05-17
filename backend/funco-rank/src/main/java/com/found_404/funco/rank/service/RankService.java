4a78c914b410bf8b764dd5370f732bad1d713d58
이선주 <sunju5402@naver.com>
2024-05-17 20:18:43 +0900

    feat: 자산별 랭킹 순위 조회 구현

--- backend/funco-rank/src/main/java/com/found_404/funco/rank/service/RankService.java
+++ backend/funco-rank/src/main/java/com/found_404/funco/rank/service/RankService.java
@@ -1,7 +1,10 @@
 package com.found_404.funco.rank.service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -13,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.found_404.funco.rank.domain.type.RankType;
 import com.found_404.funco.rank.dto.response.RankResponse;
+import com.found_404.funco.rank.dto.response.RankingResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -42,5 +46,25 @@ public class RankService {
 
 		// Page 객체 생성
 		return new PageImpl<>(rankResponses, pageable, totalElements);
+	}
+
+	public RankingResponse getRankingInfo(String type, Long memberId) {
+		Set<ZSetOperations.TypedTuple<Object>> typedTuples = rankZSetRedisTemplate.opsForZSet().reverseRangeWithScores(type,
+			0, -1);
+
+		AtomicInteger index = new AtomicInteger(0); // 인덱스를 저장할 AtomicInteger 생성
+		Optional<Long> ranking = typedTuples.stream()
+			.map(tuple -> (RankResponse)tuple.getValue())
+			.filter(Objects::nonNull)
+			.filter(rankResponse -> {
+				index.incrementAndGet(); // 인덱스 증가
+				return Objects.equals(rankResponse.member().id(), memberId); // 조건 확인
+			})
+			.findFirst()
+			.map(__ -> index.longValue());
+
+		return RankingResponse.builder()
+			.rank(ranking.orElse(null))
+			.build();
 	}
 }
