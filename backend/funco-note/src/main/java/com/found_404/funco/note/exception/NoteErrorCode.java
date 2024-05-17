bb97ccd5e55f965f74743994a619142f7c429bf3
jaeyeol <devjy0309@gmail.com>
2024-05-17 15:50:13 +0900

    fix: 게시글 상세 조회 api 에러 해결

--- backend/funco-note/src/main/java/com/found_404/funco/note/exception/NoteErrorCode.java
+++ backend/funco-note/src/main/java/com/found_404/funco/note/exception/NoteErrorCode.java
@@ -11,7 +11,11 @@ public enum NoteErrorCode {
     NOT_FOUND_NOTE(HttpStatus.BAD_REQUEST, "해당 게시글을 찾을 수 없습니다."),
     INVALID_FILTER(HttpStatus.BAD_REQUEST, "유효하지 않은 필터입니다."),
     API_SERVER_ERROR(HttpStatus.BAD_REQUEST, "외부 요청 서버 에러"),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "권한 없음.")
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "권한 없음."),
+
+    NOT_FOUND_IMAGE(HttpStatus.BAD_REQUEST, "이미지를 찾을 수 없습니다."),
+    IMAGE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드 실패"),
+    MISS_MATCH_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "이미지 타입 에러"),
     ;
 
     private final HttpStatus httpStatus;
