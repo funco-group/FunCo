6e55f9eb1fa7b4fb8b88b5d21ce9faf6a357e13b
devjy39 <devjy0309@gmail.com>
2024-05-19 01:19:24 +0900

    feat: 댓글, 대댓글 알림

--- backend/funco-note/src/main/java/com/found_404/funco/feignClient/dto/NotificationType.java
+++ backend/funco-note/src/main/java/com/found_404/funco/feignClient/dto/NotificationType.java
@@ -1,15 +1,9 @@
 package com.found_404.funco.feignClient.dto;
 
 public enum NotificationType {
-    BUY,
-    // 매도 알림
-    SELL,
-    // 정산 알림
-    SETTLE,
-    // 강제정산 알림
-    FORCE_SETTLE,
-    // 팔로우 되었다 라는 알림
-    FOLLOW,
-    // 포트폴리오가 팔렸다는 알림
-    SELL_PORTFOLIO
+    // 댓글 알림
+    NOTE_COMMENT,
+
+    // 대댓글 알림
+    COMMENT_ANSWER
 }
