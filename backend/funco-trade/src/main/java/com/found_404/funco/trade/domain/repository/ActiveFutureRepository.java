2542922db63e535619713557a0a5755f0d25df55
devjy39 <devjy0309@gmail.com>
2024-05-18 20:51:16 +0900

    feat: 선물 청산 알림 기능 추가

--- backend/funco-trade/src/main/java/com/found_404/funco/trade/domain/repository/ActiveFutureRepository.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/trade/domain/repository/ActiveFutureRepository.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public interface ActiveFutureRepository extends JpaRepository<ActiveFuture, Long>, QueryDslFuturesRepository {
+public interface ActiveFutureRepository extends JpaRepository<ActiveFuture, Long> {
     Optional<ActiveFuture> findByMemberIdAndTicker(Long memberId, String ticker);
 
     List<ActiveFuture> findAllByIdIn(Collection<Long> id);
