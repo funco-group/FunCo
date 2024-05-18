323fef915eef9a2f239457053ec0c52e3234cc4d
devjy39 <devjy0309@gmail.com>
2024-05-18 20:50:34 +0900

    feat: 선물 타입 추가

--- backend/funco-notification/src/main/java/com/found_404/funco/notification/domain/type/NotificationType.java
+++ backend/funco-notification/src/main/java/com/found_404/funco/notification/domain/type/NotificationType.java
@@ -6,12 +6,19 @@ public enum NotificationType {
 	// 매도 알림
 	SELL,
 	// 정산 알림
+
+	FUTURES,
+	// 선물 청산 알림
+
 	SETTLE,
 	// 강제정산 알림
+
 	FORCE_SETTLE,
 	// 팔로우 되었다 라는 알림
+
 	FOLLOW,
 	// 포트폴리오가 팔렸다는 알림
+
 	SELL_PORTFOLIO
 
 }
