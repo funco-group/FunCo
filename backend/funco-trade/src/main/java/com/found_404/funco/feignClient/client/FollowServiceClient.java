20276349b573dc69ada4cdc6c5649ce66a9612e0
devjy39 <devjy0309@gmail.com>
2024-05-19 01:31:58 +0900

    hotfix: api 경로 변경

--- backend/funco-trade/src/main/java/com/found_404/funco/feignClient/client/FollowServiceClient.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/feignClient/client/FollowServiceClient.java
@@ -1,7 +1,6 @@
 package com.found_404.funco.feignClient.client;
 
 import com.found_404.funco.feignClient.dto.FollowTradeRequest;
-import com.found_404.funco.feignClient.dto.UpdateCash;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +9,7 @@ import java.util.List;
 @FeignClient(name="follow-service")
 public interface FollowServiceClient {
 
-    @PatchMapping(value = "/api/v1/members/{memberId}/cash")
-    void updateCash(@PathVariable("memberId") Long memberId, @RequestBody UpdateCash updateCash);
-
-    @PostMapping()
+    @PostMapping("/api/v1/followTrades")
     void createFollowTrade(@RequestBody List<FollowTradeRequest> tradeList);
 
     @GetMapping("/api/v1/hello")
