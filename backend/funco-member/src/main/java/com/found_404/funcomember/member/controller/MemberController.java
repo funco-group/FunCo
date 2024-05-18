b2d9da351a81dd32bec56c42305abe10d6a7d031
이태호 <leetag1163@naver.com>
2024-05-18 22:58:40 +0900

    feat: member 강제 정산 MSA API 구현

--- backend/funco-member/src/main/java/com/found_404/funcomember/member/controller/MemberController.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.found_404.funcomember.member.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -124,5 +125,11 @@ public class MemberController {
 	@GetMapping("/asset/init-cash")
 	public ResponseEntity<MemberInitCashDate> getInitCashDate(@RequestParam Long memberId) {
 		return ResponseEntity.ok(memberService.readInitCashDate(memberId));
+	}
+
+	@PatchMapping("/asset/init-cash")
+	public ResponseEntity<Void> modifyCashAndInitCashDate(@RequestParam Long memberId) {
+		memberService.modifyCashAndInitCashDate(memberId);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 }
