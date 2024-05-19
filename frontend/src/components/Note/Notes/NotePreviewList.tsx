963a6268d45e33b2d71c7ff9c44af1cf5fc40b59
Hot-ttu <ghkdwndud7@naver.com>
2024-05-19 20:37:32 +0900

    feat: 노트 무한스크롤 구현

--- frontend/src/components/Note/Notes/NotePreviewList.tsx
+++ frontend/src/components/Note/Notes/NotePreviewList.tsx
@@ -5,20 +5,28 @@ import NotePreview from './NotePreview'
 interface NotePreviewListProps {
   notePreviewList: NotePreviewType[]
   setCoinList: Dispatch<SetStateAction<string[]>>
+  // eslint-disable-next-line react/require-default-props
+  lastNoteElementRef?: (node: HTMLElement | null) => void
 }
 
 function NotePreviewList({
   notePreviewList,
   setCoinList,
+  lastNoteElementRef,
 }: NotePreviewListProps) {
   return (
     <div className="mt-5 grid grid-cols-3 gap-8">
-      {notePreviewList?.map((data) => (
-        <NotePreview
-          notePreviewData={data}
+      {notePreviewList?.map((data, index) => (
+        <div
           key={data.noteId}
-          setCoinList={setCoinList}
-        />
+          ref={index === notePreviewList.length - 1 ? lastNoteElementRef : null}
+        >
+          <NotePreview
+            notePreviewData={data}
+            key={data.noteId}
+            setCoinList={setCoinList}
+          />
+        </div>
       ))}
     </div>
   )
