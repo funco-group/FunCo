1e582902e7e41e5fd8928f400619a0f37e3e5fdb
devjy39 <devjy0309@gmail.com>
2024-05-20 05:29:57 +0900

    hotfix: kafka 설정 변경 무한재시도

--- backend/funco-asset/src/main/java/com/found_404/funco/global/kafka/KafkaConsumerService.java
+++ backend/funco-asset/src/main/java/com/found_404/funco/global/kafka/KafkaConsumerService.java
@@ -1,5 +1,6 @@
 package com.found_404.funco.global.kafka;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.found_404.funco.asset.dto.request.TotalAssetHistoryRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,10 +12,23 @@ public class KafkaConsumerService {
 
     private static final String TOPIC = "history";
 
-    @KafkaListener(topics = TOPIC, groupId = "${spring.kafka.consumer.group-id}")
-    public void listen(TotalAssetHistoryRequest totalAssetHistoryRequest) {
-        log.info("kafka consume : {}", totalAssetHistoryRequest);
+//    @KafkaListener(topics = TOPIC, groupId = "${spring.kafka.consumer.group-id}")
+//    public void listen(TotalAssetHistoryRequest totalAssetHistoryRequest) {
+//        log.info("kafka consume : {}", totalAssetHistoryRequest);
+//
+//
+//    }
 
-
+    @KafkaListener(topics = "${spring.kafka.topic.name}", containerFactory = "kafkaListenerContainerFactory")
+    public void listen(String message) {
+        System.out.println("Received raw message: " + message);
+        // JSON 문자열을 직접 TotalAssetHistoryRequest 객체로 변환해 봅니다.
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            TotalAssetHistoryRequest request = mapper.readValue(message, TotalAssetHistoryRequest.class);
+            System.out.println("Deserialized message: " + request);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
