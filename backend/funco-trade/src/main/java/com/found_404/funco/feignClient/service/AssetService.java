764260e6de8f3aa6f6d780c367c0028ba2739c9c
devjy39 <devjy0309@gmail.com>
2024-05-20 01:10:54 +0900

    feat: kafka 연결, 및 토픽 전송 테스트

--- backend/funco-trade/src/main/java/com/found_404/funco/feignClient/service/AssetService.java
+++ backend/funco-trade/src/main/java/com/found_404/funco/feignClient/service/AssetService.java
@@ -3,6 +3,7 @@ package com.found_404.funco.feignClient.service;
 
 import com.found_404.funco.feignClient.client.AssetServiceClient;
 import com.found_404.funco.feignClient.dto.request.TotalAssetHistoryRequest;
+import com.found_404.funco.global.kafka.KafkaProducerService;
 import com.found_404.funco.trade.domain.FutureTrade;
 import com.found_404.funco.trade.domain.Trade;
 import com.found_404.funco.trade.domain.type.TradeType;
@@ -21,10 +22,15 @@ public class AssetService {
     private final AssetServiceClient assetServiceClient;
     private final String SERVER_NAME = "[asset-service]";
 
+    private final KafkaProducerService kafkaProducerService;
+
     @Async
     public void createAssetHistory(Trade trade, Long cash) {
+        TotalAssetHistoryRequest totalAssetHistoryRequest = TotalAssetHistoryRequest.builder().memberId(trade.getMemberId()).ticker(trade.getTicker()).tradeType(trade.getTradeType()).volume(trade.getVolume()).price(trade.getPrice()).orderCash(trade.getTradeType().equals(TradeType.BUY) ? -trade.getOrderCash() : trade.getOrderCash()).endingCash(cash).build();
+        kafkaProducerService.sendHistory(trade.getMemberId(), totalAssetHistoryRequest);
+
         try {
-            this.assetServiceClient.createCoinHistory(TotalAssetHistoryRequest.builder().memberId(trade.getMemberId()).ticker(trade.getTicker()).tradeType(trade.getTradeType()).volume(trade.getVolume()).price(trade.getPrice()).orderCash(trade.getTradeType().equals(TradeType.BUY) ? -trade.getOrderCash() : trade.getOrderCash()).endingCash(cash).build());
+            this.assetServiceClient.createCoinHistory(totalAssetHistoryRequest);
         } catch (FeignException e) {
             log.error("{} create Follow Trade By futures error : {}", SERVER_NAME, e.getMessage());
             throw new TradeException(TradeErrorCode.OTHER_SERVER_ERROR);
@@ -33,8 +39,11 @@ public class AssetService {
 
     @Async
     public void createAssetHistory(FutureTrade futureTrade, Long cash) {
+        TotalAssetHistoryRequest totalAssetHistoryRequest = TotalAssetHistoryRequest.builder().ticker(futureTrade.getTicker()).tradeType(futureTrade.getTradeType()).price(futureTrade.getPrice()).orderCash(futureTrade.getSettlement()).endingCash(cash).build();
+        kafkaProducerService.sendHistory(futureTrade.getMemberId(), totalAssetHistoryRequest);
+
         try {
-            this.assetServiceClient.createFuturesHistory(TotalAssetHistoryRequest.builder().ticker(futureTrade.getTicker()).tradeType(futureTrade.getTradeType()).price(futureTrade.getPrice()).orderCash(futureTrade.getSettlement()).endingCash(cash).build());
+            this.assetServiceClient.createFuturesHistory(totalAssetHistoryRequest);
         } catch (FeignException e) {
             log.error("{} create Follow Trade By futures error : {}", SERVER_NAME, e.getMessage());
             throw new TradeException(TradeErrorCode.OTHER_SERVER_ERROR);
