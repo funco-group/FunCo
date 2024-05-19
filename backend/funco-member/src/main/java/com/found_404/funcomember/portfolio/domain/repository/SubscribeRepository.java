5215a598888e725a0ad2fb1dfd63f12014dd3e95
devjy39 <devjy0309@gmail.com>
2024-05-20 03:26:47 +0900

    fix: 중복 구독 방지 로직 추가

--- backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/domain/repository/SubscribeRepository.java
+++ backend/funco-member/src/main/java/com/found_404/funcomember/portfolio/domain/repository/SubscribeRepository.java
@@ -1,8 +1,13 @@
 package com.found_404.funcomember.portfolio.domain.repository;
 
+import com.found_404.funcomember.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.found_404.funcomember.portfolio.domain.Subscribe;
 
+import java.util.Optional;
+
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+
+    Optional<Subscribe> findByFromMemberAndToMember(Member fromMember, Member toMember);
 }
