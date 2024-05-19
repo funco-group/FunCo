29db69b810871f987aa81db3a1abd6ce1dad914e
devjy39 <devjy0309@gmail.com>
2024-05-19 22:21:40 +0900

    feat: 통합 자산 변동내역 포트폴리오 거래 추가

---
+++ backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/dto/request/TotalAssetHistoryRequest.java
@@ -0,0 +1,26 @@
+package com.found_404.funcomember.feignClient.dto.request;
+
+import com.found_404.funcomember.feignClient.dto.AssetTradeType;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record TotalAssetHistoryRequest(
+        Long memberId,
+        AssetTradeType assetTradeType,
+        Double volume,
+        Double price,
+        Long commission,
+        Long settlement,
+        Long beginningCash,
+        Long endingCash,
+        Long orderCash,
+        String ticker,
+        String portfolioName,
+        Long investment,
+        String followName,
+        LocalDateTime followDate,
+        Double followReturnRate
+) {
+}
---
+++ backend/funco-member/src/main/java/com/found_404/funcomember/feignClient/service/AssetService.java
@@ -0,0 +1,42 @@
+
+package com.found_404.funcomember.feignClient.service;
+
+import com.found_404.funcomember.feignClient.client.AssetServiceClient;
+import com.found_404.funcomember.feignClient.dto.AssetTradeType;
+import com.found_404.funcomember.feignClient.dto.request.TotalAssetHistoryRequest;
+import com.found_404.funcomember.member.exception.MemberException;
+import com.found_404.funcomember.portfolio.domain.Subscribe;
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import static com.found_404.funcomember.member.exception.MemberErrorCode.OTHER_SERVER_ERROR;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AssetService {
+    private final AssetServiceClient assetServiceClient;
+    private final String SERVER_NAME = "[asset-service]";
+
+    @Async
+    public void createAssetHistory(Subscribe subscribe, AssetTradeType assetTradeType, Long cash) {
+        try {
+            this.assetServiceClient.createCoinHistory(TotalAssetHistoryRequest.builder()
+                    .assetTradeType(assetTradeType)
+                    .price(Double.valueOf(subscribe.getOrderCash()) *
+                            (assetTradeType.equals(AssetTradeType.SELL_PORTFOLIO) ? 1 : -1))
+                    .memberId(assetTradeType.equals(AssetTradeType.SELL_PORTFOLIO) ? subscribe.getToMember().getId() : subscribe.getFromMember().getId())
+                    .portfolioName(assetTradeType.equals(AssetTradeType.SELL_PORTFOLIO) ? subscribe.getToMember().getNickname() : subscribe.getFromMember().getNickname())
+                    .endingCash(cash)
+                    .build());
+
+        } catch (FeignException e) {
+            log.error("{} createAssetHistory error : {}", SERVER_NAME, e.getMessage());
+            throw new MemberException(OTHER_SERVER_ERROR);
+        }
+    }
+
+}
