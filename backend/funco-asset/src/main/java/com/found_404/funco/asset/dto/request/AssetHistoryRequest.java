801351c1fdd7056371ca646d8acb12677b95a40b
devjy39 <devjy0309@gmail.com>
2024-05-19 23:23:52 +0900

    hotfix: 타입 null 에러 수정

--- backend/funco-asset/src/main/java/com/found_404/funco/asset/dto/request/AssetHistoryRequest.java
+++ backend/funco-asset/src/main/java/com/found_404/funco/asset/dto/request/AssetHistoryRequest.java
@@ -3,10 +3,14 @@ package com.found_404.funco.asset.dto.request;
 import com.found_404.funco.asset.domain.type.TradeType;
 import com.found_404.funco.asset.domain.type.AssetType;
 import com.found_404.funco.asset.domain.type.PeriodType;
+import jakarta.validation.constraints.NotNull;
 
 public record AssetHistoryRequest(
-	PeriodType periodType,
-	AssetType assetType,
-	TradeType tradeType
+		@NotNull
+		PeriodType period,
+		@NotNull
+		AssetType asset,
+		@NotNull
+		TradeType trade
 ) {
 }
