9d27ec860b86f34efc775acfbaea685d664491aa
devjy39 <devjy0309@gmail.com>
2024-05-20 07:33:01 +0900

    feat: kafka 적용 프로듀서 개발

--- backend/funco-follow/src/main/java/com/found_404/funco/feignClient/service/AssetService.java
+++ backend/funco-follow/src/main/java/com/found_404/funco/feignClient/service/AssetService.java
@@ -1,46 +1,58 @@
 
 package com.found_404.funco.feignClient.service;
 
-import com.found_404.funco.feignClient.client.AssetServiceClient;
 import com.found_404.funco.feignClient.dto.AssetType;
 import com.found_404.funco.feignClient.dto.AssetTradeType;
 import com.found_404.funco.feignClient.dto.request.TotalAssetHistoryRequest;
 import com.found_404.funco.follow.domain.Follow;
-import com.found_404.funco.follow.exception.FollowException;
-import feign.FeignException;
+import com.found_404.funco.global.kafka.KafkaProducerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import static com.found_404.funco.follow.exception.FollowErrorCode.OTHER_SERVER_ERROR;
-
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class AssetService {
-    private final AssetServiceClient assetServiceClient;
+    //private final AssetServiceClient assetServiceClient;
     private final String SERVER_NAME = "[asset-service]";
+
+    private final KafkaProducerService kafkaProducerService;
 
     @Async
     public void createAssetHistory(Follow follow, AssetTradeType assetTradeType, Long cash) {
-        try {
-            this.assetServiceClient.createCoinHistory(TotalAssetHistoryRequest.builder()
-                    .assetTradeType(assetTradeType)
-                    .assetType(AssetType.FOLLOW)
-                    .memberId(assetTradeType.equals(AssetTradeType.FOLLOWING) ? follow.getFollowerMemberId() : follow.getFollowingMemberId())
-                    .investment(follow.getInvestment())
-                    .settlement(follow.getSettlement())
-                    .orderCash(follow.getSettlement())
-                    .followReturnRate(follow.getReturnRate())
-                    .commission(follow.getCommission())
-                    .endingCash(cash)
-                    .followDate(follow.getCreatedAt())
-                    .build());
-        } catch (FeignException e) {
-            log.error("{} createAssetHistory error : {}", SERVER_NAME, e.getMessage());
-            throw new FollowException(OTHER_SERVER_ERROR);
-        }
+        TotalAssetHistoryRequest totalAssetHistoryRequest = TotalAssetHistoryRequest.builder()
+                .assetTradeType(assetTradeType)
+                .assetType(AssetType.FOLLOW)
+                .memberId(assetTradeType.equals(AssetTradeType.FOLLOWING) ? follow.getFollowerMemberId() : follow.getFollowingMemberId())
+                .investment(follow.getInvestment())
+                .settlement(follow.getSettlement())
+                .orderCash(follow.getSettlement())
+                .followReturnRate(follow.getReturnRate())
+                .commission(follow.getCommission())
+                .endingCash(cash)
+                .followDate(follow.getCreatedAt())
+                .build();
+
+        kafkaProducerService.sendHistory(totalAssetHistoryRequest.memberId(), totalAssetHistoryRequest);
+//        try {
+//            this.assetServiceClient.createCoinHistory(TotalAssetHistoryRequest.builder()
+//                    .assetTradeType(assetTradeType)
+//                    .assetType(AssetType.FOLLOW)
+//                    .memberId(assetTradeType.equals(AssetTradeType.FOLLOWING) ? follow.getFollowerMemberId() : follow.getFollowingMemberId())
+//                    .investment(follow.getInvestment())
+//                    .settlement(follow.getSettlement())
+//                    .orderCash(follow.getSettlement())
+//                    .followReturnRate(follow.getReturnRate())
+//                    .commission(follow.getCommission())
+//                    .endingCash(cash)
+//                    .followDate(follow.getCreatedAt())
+//                    .build());
+//        } catch (FeignException e) {
+//            log.error("{} createAssetHistory error : {}", SERVER_NAME, e.getMessage());
+//            throw new FollowException(OTHER_SERVER_ERROR);
+//        }
     }
 
 }
